a09dc15f1cec676ab7599bdd9ff3126ad8d47249
darkred <rick3162@gmail.com>
2016-08-15 23:32:29 +0300

    Fix ESLint errors

--- thepiratebay_-_add_a_sortable_Ratio_column/thepiratebay_-_add_a_sortable_%22Ratio%22_column.user.js
+++ thepiratebay_-_add_a_sortable_Ratio_column/thepiratebay_-_add_a_sortable_%22Ratio%22_column.user.js
@@ -2,7 +2,7 @@
 // @name        thepiratebay - add a sortable "Ratio" column
 // @namespace 	darkred
 // @author      darkred
-// @description adds a sortable "Ratio" column
+// @description Adds a sortable "Ratio" column
 // @include     https://thepiratebay.se/search/*
 // @version     1.2
 // @grant       none
@@ -13,45 +13,45 @@
 
 
 function appendColumn() {
-  var tbl = document.getElementById('searchResult');
-  var newCell, newText;
-  var tr = tbl.tHead.children[0],
-  th = document.createElement('th');
-  th.innerHTML = 'Ratio';
-  th.className = 'ratioCol';
-  tr.appendChild(th);
-  var m, n;
-  for (i = 1; i < tbl.rows.length; i++) {
-    m = tbl.rows[i].cells[5].innerHTML;   // Retrieve the content of the cell of the SE column and store it to variable m
-    n = tbl.rows[i].cells[6].innerHTML;   // Retrieve the content of the cell of the LE column and store it to variable n
-    ratio = m/n;
-    if (m>0 && n==0){
-      ratio=m;
-    }
-    if (m==0 && n==0){
-      ratio=0;
-    }
-    newCell = tbl.rows[i].insertCell(-1);
-    newCell.innerHTML = (Math.round(100 * ratio) / 100);
+	var tbl = document.getElementById('searchResult');
+	var newCell;
+	var tr = tbl.tHead.children[0],
+		th = document.createElement('th');
+	th.innerHTML = 'Ratio';
+	th.className = 'ratioCol';
+	tr.appendChild(th);
+	var m, n, ratio;
+	for (var i = 1; i < tbl.rows.length; i++) {
+		m = tbl.rows[i].cells[5].innerHTML;   // Retrieve the content of the cell of the SE column and store it to variable m
+		n = tbl.rows[i].cells[6].innerHTML;   // Retrieve the content of the cell of the LE column and store it to variable n
+		ratio = m/n;
+		if (m>0 && n==0){
+			ratio=m;
+		}
+		if (m==0 && n==0){
+			ratio=0;
+		}
+		newCell = tbl.rows[i].insertCell(-1);
+		newCell.innerHTML = (Math.round(100 * ratio) / 100);
 
-  }
+	}
 }
 
 
 
 function sorting() {
-  $('table#searchResult').tablesorter({
-    headers: {
-      0: {sorter: false},
-      1: {sorter: false},
-      2: {sorter: false},
-      3: {sorter: false},
-      4: {sorter: false},
-      5: {sorter: false},
-      6: {sorter: false},
-      7: {sorter: true}
-    }
-  });
+	$('table#searchResult').tablesorter({
+		headers: {
+			0: {sorter: false},
+			1: {sorter: false},
+			2: {sorter: false},
+			3: {sorter: false},
+			4: {sorter: false},
+			5: {sorter: false},
+			6: {sorter: false},
+			7: {sorter: true}
+		}
+	});
 }
 //
 
