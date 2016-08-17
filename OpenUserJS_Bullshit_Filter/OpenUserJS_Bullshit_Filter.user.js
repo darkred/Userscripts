8638d1a29aaf404bd38ad69af5893bf903544e0e
darkred <rick3162@gmail.com>
2016-08-17 20:47:02 +0300

    Updated the 'Games' filter (added 'Bots, Diep.io, Ogar/Ogario, slither.io')

--- OpenUserJS_Bullshit_Filter/OpenUserJS_Bullshit_Filter.user.js
+++ OpenUserJS_Bullshit_Filter/OpenUserJS_Bullshit_Filter.user.js
@@ -3,7 +3,7 @@
 // @namespace 	darkred
 // @author      kuehlschrank, darkred
 // @description Hides scripts for popular browser games and social networks as well as scripts that use "foreign" characters in descriptions.
-// @version     1.1
+// @version     1.2
 // @icon        https://s3.amazonaws.com/uso_ss/icon/97145/large.png
 // @grant       none
 // @include     https://openuserjs.org/
@@ -16,104 +16,104 @@
 
 
 (function() {
-    var filters = {
-        'Games':/AntiGame|Agar|agar.io|ExtencionRipXChetoMalo|AposBot|DFxLite|ZTx-Lite|AposFeedingBot|AposLoader|Blah Blah|Orc Clan Script|Astro\s*Empires|^\s*Attack|^\s*Battle|BiteFight|Blood\s*Wars|Bots4|Brawler|\bBvS\b|Business\s*Tycoon|Castle\s*Age|City\s*Ville|Comunio|Conquer\s*Club|CosmoPulse|Dark\s*Orbit|Dead\s*Frontier|\bDOA\b|Dossergame|Dragons\s*of\s*Atlantis|Dugout|\bDS[a-z]+\n|Empire\s*Board|eRep(ublik)?|Epic.*War|ExoPlanet|Falcon Tools|Feuerwache|Farming|FarmVille|Fightinfo|Frontier\s*Ville|Ghost\s*Trapper|Gladiatus|Goalline|Gondal|Grepolis|Hobopolis|\bhwm(\b|_)|Ikariam|\bIT2\b|Jellyneo|Kapi\s*Hospital|Kings\s*Age|Kingdoms?\s*of|knastv(ö|oe)gel|Knight\s*Fight|\b(Power)?KoC(Atta?ck)?\b|\bKOL\b|Kongregate|Last\s*Emperor|Legends?\s*of|Light\s*Rising|Lockerz|\bLoU\b|Mafia\s*(Wars|Mofo)|Menelgame|Mob\s*Wars|Mouse\s*Hunt|Molehill\s*Empire|NeoQuest|MyFreeFarm|Neopets|Nemexia|\bOGame\b|Pardus|Pennergame|Pigskin\s*Empire|PlayerScripts|Popmundo|Po?we?r\s*(Bot|Tools)|PsicoTSI|Ravenwood|Schulterglatze|SpaceWars|\bSW_[a-z]+\n|\bSnP\b|The\s*Crims|The\s*West|Travian|Treasure\s*Isl(and|e)|Tribal\s*Wars|TW.?PRO|Vampire\s*Wars|War\s*of\s*Ninja|West\s*Wars|\bWoD\b|World\s*of\s*Dungeons|wtf\s*battles|Wurzelimperium/i,
-        'Social Networks': /Face\s*book|Google(\+| Plus)|\bHabbo|Kaskus|\bLepra|Leprosorium|MySpace|meinVZ|odnoklassniki|Одноклассники|Orkut|sch(ue|ü)ler(VZ|\.cc)?|studiVZ|Unfriend|Valenth|vkontakte|ВКонтакте|Qzone|Twitter/i,
-        'Non-ASCII':/[^\x00-\x7F\s]+/i,
-        'Clutter':/^\s*(.{1,3})\1+\n|^\s*(.+?)\n+\2\n*$|^\s*.{1,5}\n|do\s*n('|o)?t (install|download)|nicht installieren|just(\s*a)?\s*test|^\s*.{0,4}test.{0,4}\n|\ntest(ing)?\s*|^\s*(\{@|Smolka|Hacks)|\[\d{4,5}\]|free\s*download/i
-    };
-    if(typeof GM_getValue == 'undefined' || (typeof GM_getValue.toString == 'function' && GM_getValue.toString().indexOf('not supported') > -1)) {
-        GM_getValue = my_GM_getValue;
-        GM_setValue = my_GM_setValue;
-    }
-    insertStyle();
-    insertStatus();
-    filterScripts();
-    insertSwitches();
+	var filters = {
+		'Games': /AntiGame|Agar|agar.io|ExtencionRipXChetoMalo|AposBot|DFxLite|ZTx-Lite|AposFeedingBot|AposLoader|Blah Blah|Orc Clan Script|Astro\s*Empires|^\s*Attack|^\s*Battle|BiteFight|Blood\s*Wars|Bots|Bots4|Brawler|\bBvS\b|Business\s*Tycoon|Castle\s*Age|City\s*Ville|Comunio|Conquer\s*Club|CosmoPulse|Dark\s*Orbit|Dead\s*Frontier|Diep\.io|\bDOA\b|Dossergame|Dragons\s*of\s*Atlantis|Dugout|\bDS[a-z]+\n|Empire\s*Board|eRep(ublik)?|Epic.*War|ExoPlanet|Falcon Tools|Feuerwache|Farming|FarmVille|Fightinfo|Frontier\s*Ville|Ghost\s*Trapper|Gladiatus|Goalline|Gondal|Grepolis|Hobopolis|\bhwm(\b|_)|Ikariam|\bIT2\b|Jellyneo|Kapi\s*Hospital|Kings\s*Age|Kingdoms?\s*of|knastv(ö|oe)gel|Knight\s*Fight|\b(Power)?KoC(Atta?ck)?\b|\bKOL\b|Kongregate|Last\s*Emperor|Legends?\s*of|Light\s*Rising|Lockerz|\bLoU\b|Mafia\s*(Wars|Mofo)|Menelgame|Mob\s*Wars|Mouse\s*Hunt|Molehill\s*Empire|NeoQuest|MyFreeFarm|Neopets|Nemexia|\bOGame\b|Ogar(io)?|Pardus|Pennergame|Pigskin\s*Empire|PlayerScripts|Popmundo|Po?we?r\s*(Bot|Tools)|PsicoTSI|Ravenwood|Schulterglatze|slither\.io|SpaceWars|\bSW_[a-z]+\n|\bSnP\b|The\s*Crims|The\s*West|Travian|Treasure\s*Isl(and|e)|Tribal\s*Wars|TW.?PRO|Vampire\s*Wars|War\s*of\s*Ninja|West\s*Wars|\bWoD\b|World\s*of\s*Dungeons|wtf\s*battles|Wurzelimperium/i,
+		'Social Networks': /Face\s*book|Google(\+| Plus)|\bHabbo|Kaskus|\bLepra|Leprosorium|MySpace|meinVZ|odnoklassniki|Одноклассники|Orkut|sch(ue|ü)ler(VZ|\.cc)?|studiVZ|Unfriend|Valenth|vkontakte|ВКонтакте|Qzone|Twitter/i,
+		'Non-ASCII':/[^\x00-\x7F\s]+/i,
+		'Clutter':/^\s*(.{1,3})\1+\n|^\s*(.+?)\n+\2\n*$|^\s*.{1,5}\n|do\s*n('|o)?t (install|download)|nicht installieren|just(\s*a)?\s*test|^\s*.{0,4}test.{0,4}\n|\ntest(ing)?\s*|^\s*(\{@|Smolka|Hacks)|\[\d{4,5}\]|free\s*download/i
+	};
+	if(typeof GM_getValue == 'undefined' || (typeof GM_getValue.toString == 'function' && GM_getValue.toString().indexOf('not supported') > -1)) {
+		GM_getValue = my_GM_getValue;
+		GM_setValue = my_GM_setValue;
+	}
+	insertStyle();
+	insertStatus();
+	filterScripts();
+	insertSwitches();
 
-    var flag;
-    // ADD A NEWLINE AT THE END OF EACH FILTER
-    if (document.querySelector('.filter-status').parentNode.childNodes[5].childNodes[0].innerHTML == 'Games' ) {
-        flag = 5; }
-    else {flag = 9}
-    for (i=0; i<4; i++){
-        document.querySelector('.filter-status').parentNode.childNodes[flag].childNodes[i].innerHTML += '<br>';
-    }
+	var flag;
+	// ADD A NEWLINE AT THE END OF EACH FILTER
+	if (document.querySelector('.filter-status').parentNode.childNodes[5].childNodes[0].innerHTML == 'Games' ) {
+		flag = 5; }
+	else {flag = 9;}
+	for (var i=0; i<4; i++){
+		document.querySelector('.filter-status').parentNode.childNodes[flag].childNodes[i].innerHTML += '<br>';
+	}
 
-    // Note: you may uncomment line 45 and comment out line 46, in order the filtered scripts to be highlighted yellow -instead of hiding them- so that you can check which scripts have been filtered
-    function insertStyle() {
-        var style = document.createElement('style');
-        // style.textContent = 'tr.filtered { background-color:yellow; !important; } .filter-status { margin-left: 6px; } .filter-switches { display:none; } *:hover > .filter-switches { display:inline; } .filter-switches a { text-decoration:none !important; color:inherit; cursor:pointer; } .filter-switches a { margin-left: 8px; padding: 0 4px; } a.filter-on { background-color:#ffcccc; color:#333333 } a.filter-off { background-color:#ccffcc; color:#333333 }  ';
-        style.textContent = 'tr.filtered { display:none; !important; } .filter-status { margin-left: 6px; } .filter-switches { display:none; } *:hover > .filter-switches { display:inline; } .filter-switches a { text-decoration:none !important; color:inherit; cursor:pointer; } .filter-switches a { margin-left: 8px; padding: 0 4px; } a.filter-on { background-color:#ffcccc; color:#333333 } a.filter-off { background-color:#ccffcc; color:#333333 }  ';
-        style.type = 'text/css';
-        document.querySelector('head').appendChild(style);
-    }
-    function insertStatus() {
-    var p = document.querySelector('.col-sm-4');
-        if(p) {
-            var status = document.createElement('span');
-            status.className = 'filter-status';
-            p.appendChild(status);
-        }
-        p.innerHTML += '<br>';        // ADDS A NEWLINE AT THE END OF THE 'STATUS' ENTRY
-    }
-    function filterScripts() {
-        var activeFilters = [];
-        for(var filter in filters) {
-            if(filters.hasOwnProperty(filter) && GM_getValue(filter, 'on') == 'on') {
-                activeFilters.push(filters[filter]);
-            }
-        }
-        var nodes = document.querySelectorAll('tr'), numActiveFilters = activeFilters.length, numFiltered = 0;
-        for(var i = 1, numNodes = nodes.length-6, td = null; i < numNodes && (td = nodes[i]); i++) {
-            td.className = '';
-            for(var j = 0; j < numActiveFilters; j++) {
-                if(typeof (td.childNodes[1].childNodes[9]) !== "undefined"){                             // Check whether the current script disctription" element is not undefined, i.e. it exists
-                    if(td.childNodes[1].childNodes[9].textContent.match(activeFilters[j]) ) {
-                        td.className = 'filtered';
-                        numFiltered++;
-                        break;
-                    }
-                }
-            }
-        }
-        document.querySelector('.filter-status').textContent = document.querySelectorAll('tr').length-numFiltered-6 + ' scripts (' + numFiltered + ' filtered)';
-    }
-    function insertSwitches() {
-        var span = document.createElement('span');
-        span.className = 'filter-switches';
-        for(var filter in filters) {
-            if(filters.hasOwnProperty(filter)) {
-                span.appendChild(createSwitch(filter, GM_getValue(filter, 'on') == 'on'));
-            }
-        }
-        document.querySelector('.filter-status').parentNode.appendChild(span);
-    }
-    function createSwitch(label, isOn) {
-        var a = document.createElement('a');
-        a.className = isOn ? 'filter-on' : 'filter-off';
-        a.textContent = label;
-        a.addEventListener('click', function(e) {
-            if(this.className == 'filter-on') {
-                this.className = 'filter-off';
-                GM_setValue(this.textContent, 'off');
-            } else {
-                this.className = 'filter-on';
-                GM_setValue(this.textContent, 'on');
-            }
-            filterScripts();
-            e.preventDefault();
-        }, false);
-        return a;
-    }
-    function my_GM_setValue(name, value) {
-        localStorage.setItem(name, value);
-    }
-    function my_GM_getValue(name, defaultValue) {
-        var value;
-        if (!(value = localStorage.getItem(name))) {
-            return defaultValue;
-        }
-        return value;
-    }
+	// Note: you may uncomment line 45 and comment out line 46, in order the filtered scripts to be highlighted yellow -instead of hiding them- so that you can check which scripts have been filtered
+	function insertStyle() {
+		var style = document.createElement('style');
+		// style.textContent = 'tr.filtered { background-color:yellow; !important; } .filter-status { margin-left: 6px; } .filter-switches { display:none; } *:hover > .filter-switches { display:inline; } .filter-switches a { text-decoration:none !important; color:inherit; cursor:pointer; } .filter-switches a { margin-left: 8px; padding: 0 4px; } a.filter-on { background-color:#ffcccc; color:#333333 } a.filter-off { background-color:#ccffcc; color:#333333 }  ';
+		style.textContent = 'tr.filtered { display:none; !important; } .filter-status { margin-left: 6px; } .filter-switches { display:none; } *:hover > .filter-switches { display:inline; } .filter-switches a { text-decoration:none !important; color:inherit; cursor:pointer; } .filter-switches a { margin-left: 8px; padding: 0 4px; } a.filter-on { background-color:#ffcccc; color:#333333 } a.filter-off { background-color:#ccffcc; color:#333333 }  ';
+		style.type = 'text/css';
+		document.querySelector('head').appendChild(style);
+	}
+	function insertStatus() {
+		var p = document.querySelector('.col-sm-4');
+		if(p) {
+			var status = document.createElement('span');
+			status.className = 'filter-status';
+			p.appendChild(status);
+		}
+		p.innerHTML += '<br>';        // ADDS A NEWLINE AT THE END OF THE 'STATUS' ENTRY
+	}
+	function filterScripts() {
+		var activeFilters = [];
+		for(var filter in filters) {
+			if(filters.hasOwnProperty(filter) && GM_getValue(filter, 'on') == 'on') {
+				activeFilters.push(filters[filter]);
+			}
+		}
+		var nodes = document.querySelectorAll('tr'), numActiveFilters = activeFilters.length, numFiltered = 0;
+		for(var i = 1, numNodes = nodes.length-6, td = null; i < numNodes && (td = nodes[i]); i++) {
+			td.className = '';
+			for(var j = 0; j < numActiveFilters; j++) {
+				if(typeof (td.childNodes[1].childNodes[9]) !== 'undefined'){                             // Check whether the current script disctription" element is not undefined, i.e. it exists
+					if(td.childNodes[1].childNodes[9].textContent.match(activeFilters[j]) ) {
+						td.className = 'filtered';
+						numFiltered++;
+						break;
+					}
+				}
+			}
+		}
+		document.querySelector('.filter-status').textContent = document.querySelectorAll('tr').length-numFiltered-6 + ' scripts (' + numFiltered + ' filtered)';
+	}
+	function insertSwitches() {
+		var span = document.createElement('span');
+		span.className = 'filter-switches';
+		for(var filter in filters) {
+			if(filters.hasOwnProperty(filter)) {
+				span.appendChild(createSwitch(filter, GM_getValue(filter, 'on') == 'on'));
+			}
+		}
+		document.querySelector('.filter-status').parentNode.appendChild(span);
+	}
+	function createSwitch(label, isOn) {
+		var a = document.createElement('a');
+		a.className = isOn ? 'filter-on' : 'filter-off';
+		a.textContent = label;
+		a.addEventListener('click', function(e) {
+			if(this.className == 'filter-on') {
+				this.className = 'filter-off';
+				GM_setValue(this.textContent, 'off');
+			} else {
+				this.className = 'filter-on';
+				GM_setValue(this.textContent, 'on');
+			}
+			filterScripts();
+			e.preventDefault();
+		}, false);
+		return a;
+	}
+	function my_GM_setValue(name, value) {
+		localStorage.setItem(name, value);
+	}
+	function my_GM_getValue(name, defaultValue) {
+		var value;
+		if (!(value = localStorage.getItem(name))) {
+			return defaultValue;
+		}
+		return value;
+	}
 })();
