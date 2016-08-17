c6a4312e8dc10fef3a898ae25aee2eb3eedcd968
darkred <rick3162@gmail.com>
2016-08-17 04:03:53 +0300

    (RARBG - convert torrent timestamps to relative time) fixed the DST detection, also renamed the script

--- RARBG_-_convert_torrent_timestamps_to_relative_time/RARBG_-_convert_torrent_timestamps_to_relative_time.user.js
+++ RARBG_-_convert_torrent_timestamps_to_relative_time/RARBG_-_convert_torrent_timestamps_to_relative_time.user.js
@@ -3,7 +3,7 @@
 // @namespace   darkred
 // @description Converts torrent upload timestamps to relative time
 // @include     https://rarbg.to/torrents.php*
-// @version     2
+// @version     2.0.1
 // @grant       none
 // @require     http://code.jquery.com/jquery-2.1.4.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment-with-locales.min.js
@@ -16,31 +16,12 @@
 
 function getLocalTimezone() {
 	var tz = jstz.determine(); // Determines the time zone of the browser client
-
 	return tz.name(); // Returns the name of the time zone eg "Europe/Berlin"
 }
 
 var localTimezone = getLocalTimezone();
 var dates = $('tr.lista2 td:nth-child(3)');
 var isDST = moment().isDST();
-
-
-var currtemp;
-
-// 2016-07-21 16:51:24
-function toTimeZone(time, zone) {
-	var format0 = ('YYYY-MM-DD HH:mm:ss');
-	var format1 = ('YYYY-MM-DD HH:mm:ss Z');
-	var zz = moment(time, format1).tz(zone);
-	if (isDST) {
-		currtemp = moment(zz._i, format1);
-		currtemp = currtemp.subtract(1, 'hours').format(format0);
-		return zz.subtract(1, 'hours').fromNow();
-	} else {
-		currtemp = moment(zz._i, format1);
-		currtemp = currtemp.format(format0);
-		return zz.fromNow();}
-}
 
 
 function convertDates() {
@@ -50,8 +31,17 @@ function convertDates() {
 		temp += ' +0100';			// RARBG Timezone offset : GMT+1
 		var format0 = ('YYYY-MM-DD HH:mm:ss');
 		if (moment(dates[i].innerHTML, format0, true).isValid()) {
-			dates[i].innerHTML = toTimeZone(temp, localTimezone);
-			dates[i].title = currtemp;
+			var format1 = ('YYYY-MM-DD HH:mm:ss Z');
+			// var format2 = ('DD/MM/YYYY HH:mm:ss');
+			var format2 = ('D/M/YYYY HH:mm:ss');
+			var temp2 = moment(temp, format1).tz(localTimezone);
+			if (isDST) {
+				temp2.subtract(1, 'hours');
+			} // else {
+				// temp2.fromNow();
+			// }
+			dates[i].innerHTML = temp2.fromNow();
+			dates[i].title = temp2.format(format2);
 		}
 	}
 }
