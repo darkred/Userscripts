98070c97f4742b77fa25c31a9ac8c0b6b974bdcd
darkred <rick3162@gmail.com>
2016-08-27 18:43:49 +0300

    New script: "Userstyles - filter deleted styles in your profile"

---
+++ Userstyles_-_filter_deleted_styles_in_your_profile/README.md
@@ -0,0 +1,6 @@
+This script applies in your profile view in userstyles.org (provided that you have login).  
+It's meant for those who have also uploaded styles that they have deleted:  
+By default it shows only the active styles - optionally you may display only the deleted styles or both.  
+
+It adds these 3 links/buttons on top right. For example:  
+![image](https://i.imgur.com/4W4K8F4.jpg)  
---
+++ Userstyles_-_filter_deleted_styles_in_your_profile/Userstyles_-_filter_deleted_styles_in_your_profile.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name        Userstyles - filter deleted styles in your profile
+// @namespace   darkred
+// @include     /^https:\/\/userstyles.org\/users\/[0-9]*/
+// @description Filters deleted styles in your profile in userstyles.org
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+
+if (document.querySelector('#main-article > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)')) {		// if in your own profile
+
+	var all = document.querySelectorAll('tr').length - 1;
+	var deleted = document.querySelectorAll('.obsolete').length;
+	var scripts = all - deleted;
+
+
+	var parentElement = document.querySelector('#left-sidebar');
+
+	var theFirstChild = parentElement.firstChild;
+
+	var div = document.createElement('div');
+	parentElement.insertBefore(div, theFirstChild);
+
+
+	div.style.position = 'fixed';
+	div.style.background = 'white';
+
+	div.style.top = '210px';
+	div.style.right = '257px';
+
+
+
+
+
+	var a = document.createElement('a');
+	div.appendChild(a);
+	a.innerHTML = '<span style="width:70px;float:left;">' + 'All: ' + '</span>' + '<span style="float:right;">' + all + '</span>' + '<br/>';
+	a.onclick = toggleAll;
+
+
+	var b = document.createElement('a');
+	div.appendChild(b);
+	b.innerHTML = '<span style="width:70px;float:left;">' + 'Active: ' + '</span>' + '<span style="float:right;">' + scripts + '</span>' + '<br/>';
+	b.onclick = toggleScripts;
+
+
+	var c = document.createElement('a');
+	div.appendChild(c);
+	c.innerHTML = '<span style="width:70px;float:left;">' + 'Deleted:  ' + '</span>' + '<span style="float:right;">' + deleted  + '</span>' + '<br/>';
+	c.onclick = toggledeleted;
+
+
+	b.click();
+
+
+
+}
+
+
+
+function toggleAll(){
+	a.style.fontWeight = 'bold'; b.style.fontWeight = 'normal'; c.style.fontWeight = 'normal';
+	$('tr').show();
+	$('.obsolete').parent().show();
+}
+
+
+function toggleScripts(){
+	a.style.fontWeight = 'normal'; b.style.fontWeight = 'bold'; c.style.fontWeight = 'normal';
+	$('tr').show();
+	$('.obsolete').parent().hide();					// Initially hide the obsolete userstyles
+}
+
+
+function toggledeleted(){
+	a.style.fontWeight = 'normal'; b.style.fontWeight = 'normal'; c.style.fontWeight = 'bold';
+	$('tr').show();
+	$('tr').not($('tr .obsolete').parent()).not($('tr:first')).hide();
+
+
+}
