09282eec833d16dd33c4d432911ffbe8e9822c5d
darkred <rick3162@gmail.com>
2016-08-31 22:50:37 +0300

    Fixed indentation and removed a few unused (commented-out) lines

--- thepiratebay_helper/thepiratebay_helper.user.js
+++ thepiratebay_helper/thepiratebay_helper.user.js
@@ -3,8 +3,8 @@
 // @namespace   darkred
 // @authors     emptyparad0x, darkred
 // @description Converts dates to local timezone on thepiratebay and optionally either highlight VIP/Trusted/Moderator/Helper torrents or hide non verified torrents altogether
-// @version     0.9.5
-// @date	    2016-05-25
+// @version     0.9.5b
+// @date	    2016-08-31
 // @require	    http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require     https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @include     /^https?://thepiratebay\.(org|se|gd|la|mn|vg)/search.*$/
@@ -18,21 +18,21 @@
 
 
 /* global $:false */
-/* eslint-disable no-console, no-alert, no-mixed-spaces-and-tabs */
+/* eslint-disable no-console, no-alert*/
 /* global GM_config */
 
 
 //GM_config stuff
 GM_config.init('TPB Helper settings',{
-  timezone: {label: 'Text for Timezone:', type: 'text', cols: 10, default: 'EST'},
-  use12hour: {label: 'Use AM/PM:', type: 'checkbox', default: false},
-  am:	{label: 'Text for AM:', type: 'text', cols: 10, default: 'am'},
-  pm:	{label: 'Text for PM:', type: 'text', cols: 10, default: 'pm'},
-  tpboffset: {label: 'TPB Timezone offset    : (GMT+1)  +', type: 'int', default: 0},             // Initially it was:   tpboffset:    { label: 'TPB Timezone: GMT+', type: 'int', default: 1 },
-  showYearOnTorrentPage: {label: 'Display Year On Torrent Page:', type: 'checkbox', default: false},
-  enhanceVisibility: {label: 'Show all / Highlight trusted / Hide non-trusted:', section: ['Extras'], type: 'select', options: ['Show all', 'Highlight','Hide'], default: 'Show all'}
+	timezone: {label: 'Text for Timezone:', type: 'text', cols: 10, default: 'EST'},
+	use12hour: {label: 'Use AM/PM:', type: 'checkbox', default: false},
+	am:	{label: 'Text for AM:', type: 'text', cols: 10, default: 'am'},
+	pm:	{label: 'Text for PM:', type: 'text', cols: 10, default: 'pm'},
+	tpboffset: {label: 'TPB Timezone offset    : (GMT+1)  +', type: 'int', default: 0},             // Initially it was:   tpboffset:    { label: 'TPB Timezone: GMT+', type: 'int', default: 1 },
+	showYearOnTorrentPage: {label: 'Display Year On Torrent Page:', type: 'checkbox', default: false},
+	enhanceVisibility: {label: 'Show all / Highlight trusted / Hide non-trusted:', section: ['Extras'], type: 'select', options: ['Show all', 'Highlight','Hide'], default: 'Show all'}
 },{
-save: function(){location.reload();}});
+	save: function(){location.reload();}});
 
 $('body').append("<center><a id='TimeChangerConfig'>TPB Helper settings</a></center>");
 $('#TimeChangerConfig').click(function(){GM_config.open();}).css({'cursor': 'pointer'});
@@ -47,16 +47,16 @@ var enhanceVisibility = GM_config.get('enhanceVisibility');
 
 //Custom Date Handlers
 Date.prototype.addHours = function(h) {
-   this.setTime(this.getTime() + (h*60*60*1000));
-   return this;
+	this.setTime(this.getTime() + (h*60*60*1000));
+	return this;
 };
 Date.prototype.stdTimezoneOffset = function() {
-    var jan = new Date(this.getFullYear(), 0, 1);
-    var jul = new Date(this.getFullYear(), 6, 1);
-    return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+	var jan = new Date(this.getFullYear(), 0, 1);
+	var jul = new Date(this.getFullYear(), 6, 1);
+	return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
 };
 Date.prototype.dst = function() {
-    return this.getTimezoneOffset() < this.stdTimezoneOffset();
+	return this.getTimezoneOffset() < this.stdTimezoneOffset();
 };
 
 var d = new Date();
@@ -91,101 +91,101 @@ var dispTime = true;
 
 // Time Parsing Function For Search/Browse pages
 function parseTime(str){
-		torYear = null;
-		torMonth = null;
-		torDate = null;
-		torHour = null;
-		torMinute = null;
-		dispTime = true;
+	torYear = null;
+	torMonth = null;
+	torDate = null;
+	torHour = null;
+	torMinute = null;
+	dispTime = true;
 
-		if (str.indexOf('Today') > -1) {
+	if (str.indexOf('Today') > -1) {
 		// Format is in 'Today hh:mm'
-			torYear = tpbTodayYear;
-			torMonth = tpbTodayMonth;
-			torDate = tpbTodayDay;
-			torHour = str.slice(11,13);
-			torMinute = str.slice(14,16);
-		}
+		torYear = tpbTodayYear;
+		torMonth = tpbTodayMonth;
+		torDate = tpbTodayDay;
+		torHour = str.slice(11,13);
+		torMinute = str.slice(14,16);
+	}
 
-		else if (str.indexOf('Y-day') > -1){
+	else if (str.indexOf('Y-day') > -1){
 		// Format is in 'Y-day hh:mm'
-			torYear = tpbYdayYear;
-			torMonth = tpbYdayMonth;
-			torDate = tpbYdayDay;
-			torHour = str.slice(11,13);
-			torMinute = str.slice(14,16);
-		}
+		torYear = tpbYdayYear;
+		torMonth = tpbYdayMonth;
+		torDate = tpbYdayDay;
+		torHour = str.slice(11,13);
+		torMinute = str.slice(14,16);
+	}
 
-		else if ((str.indexOf('-') > -1) && (str.indexOf(':') > -1) && (str.length == 16)){
+	else if ((str.indexOf('-') > -1) && (str.indexOf(':') > -1) && (str.length == 16)){
 		// Format is in 'MM-DD hh:mm'
-			torYear = tpbTodayYear;
-			torMonth = str.slice(0,2)-1;
-			torDate = str.slice(3,5);
-			torHour = str.slice(11,13);
-			torMinute = str.slice(14,16);
-		}
-		else if ((str.indexOf('-') > -1) && (str.length == 15)){
+		torYear = tpbTodayYear;
+		torMonth = str.slice(0,2)-1;
+		torDate = str.slice(3,5);
+		torHour = str.slice(11,13);
+		torMinute = str.slice(14,16);
+	}
+	else if ((str.indexOf('-') > -1) && (str.length == 15)){
 		// Format is in 'MM-DD YYYY'
-			torYear = str.slice(11,15);
-			torMonth = str.slice(0,2)-1;
-			torDate = str.slice(3,5);
-			torHour = hours;
-			torMinute = '00';
-			dispTime = false;
-		}
+		torYear = str.slice(11,15);
+		torMonth = str.slice(0,2)-1;
+		torDate = str.slice(3,5);
+		torHour = hours;
+		torMinute = '00';
+		dispTime = false;
+	}
 
-		var torFullDate = new Date(torYear, torMonth, torDate, torHour, torMinute);
+	var torFullDate = new Date(torYear, torMonth, torDate, torHour, torMinute);
 
-		torFullDate.addHours(-hours);
-		var output = formatTime(torFullDate);
-		return output;
+	torFullDate.addHours(-hours);
+	var output = formatTime(torFullDate);
+	return output;
 }
 // End Time Parsing Function For Search/Browse pages
 
 // Time Format Function
 function formatTime(tempDate, time_override, year_override){
-var t_output;
-		if (tempDate.toDateString()==d.toDateString()){
-			t_output = "Today ";
+	var t_output;
+	if (tempDate.toDateString()==d.toDateString()){
+		t_output = 'Today ';
+	}
+	else if (tempDate.toDateString()==yesterday.toDateString()){
+		t_output = 'Y-day ';
+	}
+	else {
+		t_output = ('0' + (tempDate.getMonth()+1)).slice(-2) +
+			'-' + ('0' + tempDate.getDate()).slice(-2);
+
+		if ((tempDate.getFullYear() != tpbTodayYear) || (year_override)){
+			t_output = t_output + '-' + tempDate.getFullYear();
+		}
+
+		t_output += ' ';
+	}
+
+	if (dispTime || time_override){
+		if (use12hour){
+			var ampm;
+			var temph;
+			if (tempDate.getHours()>=12){
+				ampm = pm;
+				temph = (tempDate.getHours()-12);
 			}
-		else if (tempDate.toDateString()==yesterday.toDateString()){
-			t_output = "Y-day ";
+			else {
+				ampm = am;
+				temph = (tempDate.getHours());
+			}
+			if (temph===0){
+				temph = 12;
+			}
+			tempDate.setHours(temph);
+			t_output = t_output + tempDate.getHours() + ':' + ('0' + tempDate.getMinutes()).slice(-2) + ampm;
 		}
 		else {
-			t_output = ("0" + (tempDate.getMonth()+1)).slice(-2) +
-			"-" + ("0" + tempDate.getDate()).slice(-2);
-
-			if ((tempDate.getFullYear() != tpbTodayYear) || (year_override)){
-				t_output = t_output + "-" + tempDate.getFullYear();
-			}
-
-			t_output += " ";
-			}
-
-		if (dispTime || time_override){
-			if (use12hour){
-				var ampm;
-				var temph;
-				if (tempDate.getHours()>=12){
-					ampm = pm;
-					temph = (tempDate.getHours()-12);
-					}
-				else {
-					ampm = am;
-					temph = (tempDate.getHours());
-				}
-				if (temph===0){
-					temph = 12;
-				}
-				tempDate.setHours(temph);
-				t_output = t_output + tempDate.getHours() + ":" + ("0" + tempDate.getMinutes()).slice(-2) + ampm;
-				}
-			else {
-				t_output = t_output + ("0" + tempDate.getHours()).slice(-2) + ":" + ("0" + tempDate.getMinutes()).slice(-2);
-				}
-			t_output = t_output + ' ' + timezone;
-			}
-		return t_output;
+			t_output = t_output + ('0' + tempDate.getHours()).slice(-2) + ':' + ('0' + tempDate.getMinutes()).slice(-2);
+		}
+		t_output = t_output + ' ' + timezone;
+	}
+	return t_output;
 }
 // End Time Format Function
 
@@ -205,7 +205,7 @@ function parseTimeString(str){
 //Change the time on the footer
 $('p#footer, div#stats').each(function(){
 	var f_str = $(this).html();
-	var f_strTime;
+	// var f_strTime;
 	var f_pos = f_str.indexOf('Last updated');
 	if (f_pos > -1){
 		f_pos += 13;
@@ -219,7 +219,7 @@ $('p#footer, div#stats').each(function(){
 		f_footerDate.addHours(-hours);
 		f_strTime = formatTime(f_footerDate,true);
 		$(this).html(f_startString + f_strTime + f_endString);
-		}
+	}
 
 });
 
@@ -228,15 +228,15 @@ var url = window.location.href;
 var host = window.location.host;
 if (url.indexOf(host + '/torrent/') != -1) {
 	//Change time on the torrent page
-	var torPageFullDate = parseTimeString($("dt:contains('Uploaded')").next().html());
+	var torPageFullDate = parseTimeString($('dt:contains(\'Uploaded\')').next().html());
 	torPageFullDate.addHours(-hours);
-	$("dt:contains('Uploaded')").next().html(formatTime(torPageFullDate,true, showYearOnTorrentPage));
+	$('dt:contains(\'Uploaded\')').next().html(formatTime(torPageFullDate,true, showYearOnTorrentPage));
 	jQuery.noConflict(); //Need this to run last in order to fix problem with thepiratebay comments
    }
 else {
 	// Change the times on the browse/search pages
 	var allBody = document.body.innerHTML;
-	if (allBody.indexOf("view=s") > -1){
+	if (allBody.indexOf('view=s') > -1){
 		// Page is in Double View
 		$('table#searchResult font').each(function(){
 			var str = $(this).html();
@@ -246,17 +246,17 @@ else {
 				if (strTime.indexOf('min')==-1) {
 					//Do nothing with times displayed in "n Mins ago"
 					var strLast = str.slice(c_loc, str.length);
-					$(this).html("Uploaded " + parseTime(strTime) + strLast);
+					$(this).html('Uploaded ' + parseTime(strTime) + strLast);
 				}
 			}
 		});
 	}
-	else if (allBody.indexOf("view=d") > -1){
+	else if (allBody.indexOf('view=d') > -1){
 		// Page is in Single View
 		$('table#searchResult td:nth-child(3)').each(function(){
 			if ($(this).html().indexOf('min')==-1){
 			//Do nothing with times displayed in "n Mins ago"
-			$(this).html(parseTime($(this).html()));
+				$(this).html(parseTime($(this).html()));
 			}
 		});
 	}
@@ -272,32 +272,32 @@ else {
 
 
 	// Hide non trusted torrents
-    var counter = 0;
+	var counter = 0;
 	if (enhanceVisibility == 'Hide') {
 
 		hideNonTrusted();
 //     alert (counter);
-    if (counter > 0) {
-      document.querySelector('#TimeChangerConfig').insertAdjacentHTML('afterend', '<br><i>' + counter  + ' non verified torrents hidden - click here or press ` to view all torrents</i>');
-    }
+		if (counter > 0) {
+			document.querySelector('#TimeChangerConfig').insertAdjacentHTML('afterend', '<br><i>' + counter  + ' non verified torrents hidden - click here or press ` to view all torrents</i>');
+		}
 		var flagHide = true;
 
 
 
 		function toggleHide() {
 			if (counter > 0) {
-        if (flagHide == true) {
-          $('table#searchResult tbody tr').each(function() {
-            $(this).children('td').show();
-          });
-				  $('#TimeChangerConfig').nextAll().eq(1).html('<i>click here (or press `) to view only verified torrents</i>');
-				  flagHide = false;
-        } else {
-          counter = 0;
-          hideNonTrusted();
-          $('#TimeChangerConfig').nextAll().eq(1).html('<i>' + counter  + ' non verified torrents hidden - click here or press ` to view all torrents</i>');
-          flagHide = true;
-        }
+				if (flagHide == true) {
+					$('table#searchResult tbody tr').each(function() {
+						$(this).children('td').show();
+					});
+					$('#TimeChangerConfig').nextAll().eq(1).html('<i>click here (or press `) to view only verified torrents</i>');
+					flagHide = false;
+				} else {
+					counter = 0;
+					hideNonTrusted();
+					$('#TimeChangerConfig').nextAll().eq(1).html('<i>' + counter  + ' non verified torrents hidden - click here or press ` to view all torrents</i>');
+					flagHide = true;
+				}
 			}
 
 		}
@@ -334,21 +334,21 @@ else {
 
 
 function hideNonTrusted() {
-    var total = 0;
-    // var counter = 0;
-    $('table#searchResult tbody tr').each(function() {
-        total++;
-        if (($(this).html().indexOf('title="VIP"') > -1) ||
-            ($(this).html().indexOf('title="Trusted"') > -1) ||
-            ($(this).html().indexOf('title="Moderator"') > -1) ||
-            ($(this).html().indexOf('Helper') > -1) ||
-            ($(this).html().indexOf('src="https://piratebay.org/img/next.gif"') > -1)) {
-            //do nothing
-        } else {
-            $(this).children('td').hide();
-            counter++;
-        }
-    });
+	// var total = 0;
+	// var counter = 0;
+	$('table#searchResult tbody tr').each(function() {
+		// total++;
+		if (($(this).html().indexOf('title="VIP"') > -1) ||
+			($(this).html().indexOf('title="Trusted"') > -1) ||
+			($(this).html().indexOf('title="Moderator"') > -1) ||
+			($(this).html().indexOf('Helper') > -1) ||
+			($(this).html().indexOf('src="https://piratebay.org/img/next.gif"') > -1)) {
+			//do nothing
+		} else {
+			$(this).children('td').hide();
+			counter++;
+		}
+	});
 
 }
 
@@ -356,34 +356,34 @@ function hideNonTrusted() {
 
 function highlight() {
 
-    $('table#searchResult tbody tr').each(function() {
-        if ($(this).html().indexOf('title="VIP"') > -1) {
-            $(this).children('td').css({
-                'background-color': '#CFFECD'
-            });
-        } else if ($(this).html().indexOf('title="Trusted"') > -1) {
-            $(this).children('td').css({
-                'background-color': '#F9D5DB'
-            }); // (initially it was FECDFE)   and then FECDD9
-        } else if ($(this).html().indexOf('title="Moderator"') > -1) {
-            $(this).children('td').css({
-                'background-color': '#DCDCDC'
-            });
-        } else if ($(this).html().indexOf('Helper') > -1) { // Extra line
-            $(this).children('td').css({
-                'background-color': '#33CCCC'
-            });
-        } else if ($(this).html().indexOf('src="https://piratebay.org/img/next.gif"') > -1) {
-            //do nothing
-        } else {
-            $(this).children('td').css({
-                'opacity': '0.5'
-            });
-            $(this).children('td').css({
-                'background-color': 'white'
-            });
-        }
-    });
+	$('table#searchResult tbody tr').each(function() {
+		if ($(this).html().indexOf('title="VIP"') > -1) {
+			$(this).children('td').css({
+				'background-color': '#CFFECD'
+			});
+		} else if ($(this).html().indexOf('title="Trusted"') > -1) {
+			$(this).children('td').css({
+				'background-color': '#F9D5DB'
+			}); // (initially it was FECDFE)   and then FECDD9
+		} else if ($(this).html().indexOf('title="Moderator"') > -1) {
+			$(this).children('td').css({
+				'background-color': '#DCDCDC'
+			});
+		} else if ($(this).html().indexOf('Helper') > -1) { // Extra line
+			$(this).children('td').css({
+				'background-color': '#33CCCC'
+			});
+		} else if ($(this).html().indexOf('src="https://piratebay.org/img/next.gif"') > -1) {
+			//do nothing
+		} else {
+			$(this).children('td').css({
+				'opacity': '0.5'
+			});
+			$(this).children('td').css({
+				'background-color': 'white'
+			});
+		}
+	});
 
 
 }
