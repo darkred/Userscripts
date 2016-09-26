3953f6f42d41e4042f345ef3ed5142f706c5a869
darkred <rick3162@gmail.com>
2016-09-26 14:09:59 +0300

    (RARBG - torrent and magnet links in search results) Improved @include entries (now it also works in torrent category view and in top 10). Also, for some torrent categories the magnet links cannot be generated: now this won't prevent the script from displaying the direct links.

--- RARBG_-_torrent_and_magnet_links_in_search_results/RARBG_-_torrent_and_magnet_links_in_search_results.user.js
+++ RARBG_-_torrent_and_magnet_links_in_search_results/RARBG_-_torrent_and_magnet_links_in_search_results.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        RARBG - torrent and magnet links in search results
 // @namespace   darkred
-// @description	Adds a column in search results for direct torrent or magnet link downloading
-// @include     https://rarbg.to/torrents.php?search=*
-// @version     1
-// @require		https://code.jquery.com/jquery-3.1.1.min.js
+// @description Adds a column in search results for direct torrent or magnet link downloading
+// @include     https://rarbg.to/torrents.php*
+// @include     https://rarbg.to/top10
+// @version     1.1
+// @require     https://code.jquery.com/jquery-3.1.1.min.js
 // @grant       GM_getResourceURL
 // @resource    icon1 https://dyncdn.me/static/20/img/16x16/download.png
 // @resource    icon2 https://dyncdn.me/static/20/img/magnet.gif
@@ -23,7 +24,7 @@ function appendColumn(title) {
 	columnCells.attr('class', 'lista');
 	columnCells.attr('width', '50px');
 	columnCells.attr('align', 'center');
-	
+
 	arr1 = $('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');		// new column
 	arr2 = $('.lista2t > tbody > tr[class="lista2"] > td:nth-child(2)');		// old column
 }
@@ -31,17 +32,23 @@ function appendColumn(title) {
 
 function addLinks(){
 	$.each(arr1, function( index ) {
-		var hash = arr2[index].firstChild.outerHTML.match(/over\/(.*)\.jpg\\/)[1];
+		if ((/over\/(.*)\.jpg\\/).test(arr2[index].firstChild.outerHTML)){
+			var hash = arr2[index].firstChild.outerHTML.match(/over\/(.*)\.jpg\\/)[1];
+		}
 		var title = arr2[index].firstChild.innerText;
 		var trackers = 'http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710&tr=udp%3A%2F%2F9.rarbg.to%3A2710';
-		
+
 		arr1[index].innerHTML = '<a href="' + arr2[index].firstChild.href.replace('torrent/', 'download.php?id=') + '&f=' + arr2[index].firstChild.innerText + '-[rarbg.com].torrent">' + '<img src="'+ GM_getResourceURL('icon1') + '">' + '</>';
-		arr1[index].innerHTML += '   <a href="magnet:?xt=urn:btih:' + hash + '&dn=' + title + '&tr=' + trackers + ' ">' + '<img src="'+ GM_getResourceURL('icon2') + '">' + '</>';
+		if (hash !== undefined){
+			arr1[index].innerHTML += '&nbsp;<a href="magnet:?xt=urn:btih:' + hash + '&dn=' + title + '&tr=' + trackers + ' ">' + '<img src="'+ GM_getResourceURL('icon2') + '">' + '</>';
+		} else {
+			arr1[index].innerHTML += '&nbsp;&nbsp;&nbsp;&nbsp;';
+		}
 	});
 }
 
 
 var arr1, arr2;
 
-appendColumn('DL/ML');
+appendColumn('DL&nbsp;ML');
 addLinks();
