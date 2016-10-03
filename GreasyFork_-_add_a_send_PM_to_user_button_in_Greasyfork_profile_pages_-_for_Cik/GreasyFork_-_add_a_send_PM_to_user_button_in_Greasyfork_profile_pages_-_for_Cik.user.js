9d3837cd849a423f9312a3c1b2e3de851cc84345
darkred <rick3162@gmail.com>
2016-10-04 01:28:15 +0300

    (GreasyFork - add a 'send PM to user' button in Greasyfork profile pages - for Citrus GFork) fix for the recipient username being displayed truncated

--- GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages_-_for_Cik/GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages_-_for_Cik.user.js
+++ GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages_-_for_Cik/GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages_-_for_Cik.user.js
@@ -4,20 +4,20 @@
 // @description It adds a 'send PM to user' button in Greasyfork profile pages
 // @include     https://greasyfork.org/en/users/*
 // @include     https://greasyfork.org/en/forum/messages/add
-// @version     2016.10.03
+// @version     2016.10.04
 // @grant       GM_getResourceURL
 // @resource    icon http://i.imgur.com/ZU0xS0c.jpg
 // @run-at      document-idle
 // ==/UserScript==
 
 if (document.querySelector('.user-profile-link > a:nth-child(1)') !== null){
-	var yourProfileName = document.querySelector('.user-profile-link > a:nth-child(1)').innerHTML !== null;
+	var yourProfileName = document.querySelector('.user-profile-link > a:nth-child(1)').innerHTML;
 }
 if (document.querySelector('#user-profile > h1:nth-child(1)') !== null){
 	var currentProfileName = document.querySelector('#user-profile > h1:nth-child(1)').textContent.replace('\'s Profile', '');
 }
-if (window.location.href.indexOf('users') > -1 // if current URL is a profile page
-	&& yourProfileName != currentProfileName) { // ... and this profile page is not yours
+if (window.location.href.indexOf('users') !== -1 // if current URL is a profile page
+	&& yourProfileName !== currentProfileName) { // ... and this profile page is not yours
 	sessionStorage.setItem('recipient', currentProfileName); // store in sessionStorage the profileName (it will be inserted in the 'Recipients' textbox ) -after you press the button-
 	var referenceNode = document.querySelector('#user-profile > h1:nth-child(1)');
 	var a = document.createElement('input');
@@ -32,7 +32,6 @@ if (window.location.href.indexOf('users') > -1 // if current URL is a profile pa
 	});
 }
 if (window.location.href.indexOf('messages') !== -1) { // if current URL is a 'send PM' page
-	// document.querySelector('#token-input-Form_To').value = sessionStorage.getItem('recipient') ; // .. then insert the stored value of 'recipient' in the 'Recipients' textbox
-	document.querySelector('.token-input-token > p:nth-child(1)').innerText = sessionStorage.getItem('recipient') ; // .. then insert the stored value of 'recipient' in the 'Recipients' textbox
-	document.querySelector('#Form_Body').focus();
+	document.querySelector('#token-input-Form_To').style.width = 'auto';
+	document.querySelector('#token-input-Form_To').value = sessionStorage.getItem('recipient');
 }
