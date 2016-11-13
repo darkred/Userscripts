31fdd09f5b9c86d03050e063b343947d92a3cac1
darkred <rick3162@gmail.com>
2016-11-13 21:00:13 +0200

    (GreasyFork - add a 'send PM to user' button in Greasyfork profile pages - for Citrus GFork) Now using 'jquery-simulate-ext' (and its required libraries) in order the username to be "typed" (as a simulated keypress sequence) in the autocomplete popup, and so the script to be able to click it (to select it).

--- GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages_-_for_Cik/GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages_-_for_Cik.user.js
+++ GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages_-_for_Cik/GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages_-_for_Cik.user.js
@@ -4,16 +4,22 @@
 // @description It adds a 'send PM to user' button in Greasyfork profile pages
 // @include     https://greasyfork.org/en/users/*
 // @include     https://greasyfork.org/en/forum/messages/add
-// @version     2016.10.04
+// @version     2016.11.12
 // @grant       GM_getResourceURL
 // @resource    icon http://i.imgur.com/ZU0xS0c.jpg
 // @run-at      document-idle
+// @require     https://greasyfork.org/scripts/24818-bililiterange/code/bililiteRange.js
+// @require     https://code.jquery.com/jquery-3.1.1.min.js
+// @require     https://greasyfork.org/scripts/24819-jquery-simulate/code/jquery-simulate.js
+// @require     https://greasyfork.org/scripts/24820-jquery-simulate-ext/code/jquery-simulate-ext.js
+// @require     https://greasyfork.org/scripts/24822-jquery-simulate-key-sequence-js/code/jquerysimulatekey-sequencejs.js
+// @require     https://greasyfork.org/scripts/21927-arrive-js/code/arrivejs.js?version=139586
 // ==/UserScript==
 
-if (document.querySelector('.user-profile-link > a:nth-child(1)') !== null){
+if (document.querySelector('.user-profile-link > a:nth-child(1)') !== null) {
 	var yourProfileName = document.querySelector('.user-profile-link > a:nth-child(1)').innerHTML;
 }
-if (document.querySelector('#user-profile > h1:nth-child(1)') !== null){
+if (document.querySelector('#user-profile > h1:nth-child(1)') !== null) {
 	var currentProfileName = document.querySelector('#user-profile > h1:nth-child(1)').textContent.replace('\'s Profile', '');
 }
 if (window.location.href.indexOf('users') !== -1 // if current URL is a profile page
@@ -24,14 +30,31 @@ if (window.location.href.indexOf('users') !== -1 // if current URL is a profile 
 	referenceNode.appendChild(a);
 	a.style.padding = '0px 12px';
 	a.setAttribute('type', 'image');
-	a.setAttribute('src', GM_getResourceURL('icon') );
+	a.setAttribute('src', GM_getResourceURL('icon'));
 	a.id = 'pmButton';
 	a.title = 'Send PM to ' + currentProfileName;
 	document.getElementById('pmButton').addEventListener('click', function() {
 		window.open('https://greasyfork.org/en/forum/messages/add', '_self');
 	});
 }
+
 if (window.location.href.indexOf('messages') !== -1) { // if current URL is a 'send PM' page
+	document.querySelector('#token-input-Form_To').focus();
 	document.querySelector('#token-input-Form_To').style.width = 'auto';
-	document.querySelector('#token-input-Form_To').value = sessionStorage.getItem('recipient');
+
+	// 	document.querySelector('#token-input-Form_To').value = sessionStorage.getItem('recipient');
+	var recipient = sessionStorage.getItem('recipient');
+	$('#token-input-Form_To').simulate('key-sequence', { sequence: recipient, delay: 1 });
+
+	document.arrive('.token-input-selected-dropdown-item', function () {
+		triggerMouseEvent (document.querySelector('.token-input-selected-dropdown-item'), 'mousedown');
+	});
+
+}
+
+
+function triggerMouseEvent (node, eventType) {
+	var clickEvent = document.createEvent ('MouseEvents');
+	clickEvent.initEvent (eventType, true, true);
+	node.dispatchEvent (clickEvent);
 }
---
+++ GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages_-_for_Cik/README.md
@@ -0,0 +1,32 @@
+By default you may send a pm via a user's forum profile page (e.g. see link 1).  
+With this script you may also send a pm via user's Greasyfork profile page (e.g. see link 2).
+```
+Forum profile:      https://greasyfork.org/en/forum/profile/324/darkred
+Greasyfork profile: https://greasyfork.org/en/users/2160-darkred
+```
+
+example:  
+![](https://i.imgur.com/62nYnl2.gif)
+
+*Notes: the button doesn't appear in your own Greasyfork profile page, 
+and it only works when you are logged in, for obvious reasons*. 
+<br>
+
+**<u>Update 11/12/2016</u>**:
+The script was not working ok (because you have to simulate keypresses -with a delay between each press-  
+in order the username to be "typed" in the autocomplete popup, and so the script to be able to click it, to select it).  
+I fixed it, but, in order to manage sending this simulated keypress sequence ( 1-line in the script! - line 47)   
+I have to use the following `require`'s :  
+- `jQuery`,  
+- 1 from [bililiteRange](https://github.com/dwachss/bililiteRange),   
+- 1 from [jquery-simulate](https://github.com/jquery/jquery-simulate) (by jQuery), and   
+- 2 from [jquery-simulate-ext](https://github.com/j-ulrich/jquery-simulate-ext) .  
+
+And, I also use [arrive.js](https://github.com/uzairfarooq/arrive) in the script.   
+
+The fact that these `require`'s are needed in order by the `jquery-simulate-ext` plugin to work,  
+is documented here: [https://github.com/j-ulrich/jquery-simulate-ext#usage](https://github.com/j-ulrich/jquery-simulate-ext#usage).
+
+<br>
+
+[Hosted in GitHub](https://github.com/darkred/Userscripts)
