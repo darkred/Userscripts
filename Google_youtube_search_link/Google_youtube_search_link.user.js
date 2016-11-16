cf0116a4deaadd832b839e569fd7fcbaf2e232e0
darkred <rick3162@gmail.com>
2016-11-16 19:41:59 +0200

    (Google youtube search link) Changed 2 selectors

--- Google_youtube_search_link/Google_youtube_search_link.user.js
+++ Google_youtube_search_link/Google_youtube_search_link.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Google youtube search link
 // @description  Adds a Youtube search link next to the Videos link (e.g. Web, Images, Videos, Youtube, News, Maps, Shopping, ...)
-// @version      1.2
+// @version      2016.11.16
 // @author       wOxxOm, darkred
 // @namespace    darkred
 // @license      MIT License
@@ -16,13 +16,15 @@ process();
 new MutationObserver(process).observe(document, { childList: true, subtree: true });
 
 function process(mutations) {
-	var youtube = document.getElementById('__YOUTUBE_SEARCH__');
+	var youtube = document.querySelector('#__YOUTUBE_SEARCH__');
 	if (youtube)
 		return;
 
 	var menu = document.querySelector('#hdtb-msb');			// selector for the element that contains all the links (Web, Images, Videos, News, Maps, Shopping, ...)
 	if (!menu)
 		return;
+
+	var menuContainer = menu.querySelector('.hdtb-imb').parentNode;
 
 	if (!youtube) {
 		var q = '',
@@ -31,7 +33,7 @@ function process(mutations) {
 			if (queryElement.value)
 				q = encodeURIComponent(queryElement.value);
 			else {
-				new MutationObserver(function (mut) {
+				new MutationObserver(function(mut) {
 					if (queryElement.value) {
 						var youtube = document.querySelector('#__YOUTUBE_SEARCH__');
 						if (youtube) {
@@ -45,10 +47,11 @@ function process(mutations) {
 			q = q[1];
 
 		var node = $(`a[href*='tbm=vid']`);			// selector (jQuery) for the 'Videos' link (works in any Google search page language)
-		$(node).after(
+		$(node.parent()).after(
 			'<div class="hdtb-mitem hdtb-imb" id="__YOUTUBE_SEARCH__">' +
 			'<a class="q qs" href="https://www.youtube.com/results?search_query=' + q + '">Youtube</a>' +
 			'</div>');			// insert the YouTube link (via jQuery's after() )
 	}
-	new MutationObserver(process).observe(node, { childList: true });
+
+	new MutationObserver(process).observe(menuContainer, { childList: true });
 }
