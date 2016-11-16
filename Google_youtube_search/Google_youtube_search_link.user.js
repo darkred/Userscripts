d44db4bcd18d0dbf45be1fe42d368d3a461ce3d7
darkred <rick3162@gmail.com>
2016-11-16 15:05:51 +0200

    (Google youtube search link) Initial commit

---
+++ Google_youtube_search/Google_youtube_search_link.user.js
@@ -0,0 +1,54 @@
+﻿// ==UserScript==
+// @name         Google youtube search link
+// @description  Adds a Youtube search link next to the Videos link (e.g. Web, Images, Videos, Youtube, News, Maps, Shopping, ...)
+// @version      1.2
+// @author       wOxxOm, darkred
+// @namespace    wOxxOm.scripts
+// @license      MIT License
+// @include      https://www.google.com/*
+// @include      /https?:\/\/(www\.)?google\.(com|(?:com?\.)?\w\w)\/.*/
+// @grant        none
+// @run-at       document-start
+// @require      https://code.jquery.com/jquery-3.1.1.min.js
+// ==/UserScript==
+
+process();
+new MutationObserver(process).observe(document, { childList: true, subtree: true });
+
+function process(mutations) {
+	var youtube = document.getElementById('__YOUTUBE_SEARCH__');
+	if (youtube)
+		return;
+
+	var menu = document.querySelector('#hdtb-msb');			// selector for the element that contains all the links (Web, Images, Videos, News, Maps, Shopping, ...)
+	if (!menu)
+		return;
+
+	if (!youtube) {
+		var q = '',
+			queryElement = document.querySelector('input[name="q"]');		// selector for the Google search input textbox
+		if (queryElement) {
+			if (queryElement.value)
+				q = encodeURIComponent(queryElement.value);
+			else {
+				new MutationObserver(function (mut) {
+					if (queryElement.value) {
+						var youtube = document.querySelector('#__YOUTUBE_SEARCH__');
+						if (youtube) {
+							this.disconnect();
+							youtube.querySelector('a').href += encodeURIComponent(queryElement.value);
+						}
+					}
+				}).observe(queryElement, { attributes: true });			// monitor the textbox for changes (your typed criteria)
+			}
+		} else if ((q = location.href.match(/^.+?(?:[#\/&?](?:q|query))=(.+?)(?:|&.+|\|.+)$/)))
+			q = q[1];
+
+		var node = $(`a[href*='tbm=vid']`);			// selector (jQuery) for the 'Videos' link (works in any Google search page language)
+		$(node).after(
+			'<div class="hdtb-mitem hdtb-imb" id="__YOUTUBE_SEARCH__">' +
+			'<a class="q qs" href="https://www.youtube.com/results?search_query=' + q + '">Youtube</a>' +
+			'</div>');			// insert the YouTube link (via jQuery's after() )
+	}
+	new MutationObserver(process).observe(node, { childList: true });
+}
---
+++ Google_youtube_search/README.md
@@ -0,0 +1,34 @@
+This script is a fork of [Google youtube search link](https://greasyfork.org/en/scripts/7784-google-youtube-search-link) by wOxxOm.  
+Thanks to wOxxOm for the initial version.
+
+---
+
+This script applies to Google search (using any language).
+
+It inserts a `YouTube` link next to the `Videos` one, i.e. `Videos | YouTube`  
+in order to search for the given criteria in YouTube.   
+_It doesn't rearrange the rest links (see note)._ 
+
+
+i.e.  if you type something in google search that looks like you're searching for _images_,
+the links will become:  
+`All | Images | Videos | YouTube | etc`
+
+If it looks like you're looking for _videos_, the links will become:  
+`All | Videos | YouTube | Images | etc`
+
+If you type something that looks like an _address_, the links will become:  
+`All | Maps | Images | Videos | YouTube | News | More`
+
+If you type something that's related to some recent _news_, the links will become:  
+`All | News | Images | Videos | YouTube | Maps | More`
+
+And, if you type a _book_ title then the links will become:    
+`All | Images | Videos | YouTube | Books | News | More`
+
+
+<br>
+
+_Note: The feature of the initial script to re-arrange Images, Videos, Youtube links to always be on 2nd, 3rd, 4th places,
+that was applied by default, 
+was breaking the Google's default tabs order (which takes into account the kind of search criteria that you have entered) is removed in this fork._
