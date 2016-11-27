b589032c119af0d384ce33b28def7f556e5bbcdf
darkred <rick3162@gmail.com>
2016-11-27 17:39:15 +0200

    (Twitter - add unread notifications count in the tab title) Fix for when viewing profile pages, it would only display "Twitter" as title Also, removed two unused forEach

--- Twitter_-_add_unread_notifications_count_in_the_tab_title/Twitter_-_add_unread_notifications_count_in_the_tab_title.user.js
+++ Twitter_-_add_unread_notifications_count_in_the_tab_title/Twitter_-_add_unread_notifications_count_in_the_tab_title.user.js
@@ -4,20 +4,29 @@
 // @author      darkred
 // @description Adds unread notifications count in the tab title
 // @include     https://twitter.com/*
-// @version     2016.10.24
+// @version     2016.11.27
 // @grant       none
 // @require     https://greasyfork.org/scripts/21927-arrive-js/code/arrivejs.js?version=139586
 // ==/UserScript==
 
+
 var counter;
 
 function addCounterInTitle() {
-	// alert();
-	counter = parseInt(document.querySelector('.count-inner').innerHTML);
-	if (counter > 0 && document.title.indexOf('|') === - 1) {
-		document.title = counter + ' | ' + document.title;
+	// alert(0);
+	counter = parseInt(document.querySelector('.count-inner').innerHTML);					// the Notifications counter value
+	// if (counter > 0 && document.title.indexOf('|') > 3) {		// if the '|' symbol is the default separator of username and 'Twitter' when viewing profiles, e.g.:  Twitter Support (@Support) | Twitter. In here the position of `|` is 27.
+	if (counter > 0) {		// if the '|' symbol is the default separator of username and 'Twitter' when viewing profiles, e.g.:  Twitter Support (@Support) | Twitter. In here the position of `|` is 27.
+		if (/[0-9]+\ \|\ .*/.test(document.title)){			// if our counter is already added to title
+			var defaultTitle = document.title.match(/[0-9]+\ \|\ (.*)/)[1];
+			document.title = counter + ' | ' + defaultTitle;
+			return;
+		} else {
+			document.title = counter + ' | ' + document.title;				// add the counter to the title
+			return;
+		}
 	} else if (counter === 0) {
-		document.title = /[0-9]*\ \|\ (.*)/g.exec(document.title) [1];
+		document.title = /[0-9]+\ \|\ (.*)/g.exec(document.title)[1];		// remove title's added counter
 	}
 }
 
@@ -34,9 +43,7 @@ document.arrive('div.js-account-summary:nth-child(1) > div:nth-child(2) > a:nth-
 document.arrive('.new-tweets-bar', function () {
 	var target = document.querySelector('.new-tweets-bar'); // ..οbserve the unread counter for changes(increase)
 	var observer = new MutationObserver(function (mutations) {
-		mutations.forEach(function (mutation) {
-			addCounterInTitle(); 							// Refresh the counter on every such change
-		});
+		addCounterInTitle(); 							// Refresh the counter on every such change
 	});
 	var config = {
 		childList: true,
@@ -56,7 +63,7 @@ document.leave('.new-tweets-bar', function () {
 // Whenever viewing the 'Notifications' tab
 document.arrive('.NotificationsHeadingContent', function () {
 	document.querySelector('.count-inner').innerHTML = 0;			// ..reset the counter..
-	document.title = document.title.match(/[0-9]*\ \|\ (.*)/)[1];	// ..and the tab title
+	document.title = document.title.match(/[0-9]+\ \|\ (.*)/)[1];	// ..and the tab title
 });
 
 
@@ -64,9 +71,7 @@ document.arrive('.NotificationsHeadingContent', function () {
 // Observe the 'Notifications' counter for changes
 var target2 = document.querySelector('.count-inner');
 var observer2 = new MutationObserver(function (mutations) {
-	mutations.forEach(function (mutation) {
-		addCounterInTitle();
-	});
+	addCounterInTitle();
 });
 var config2 = {
 	childList: true,
@@ -82,7 +87,7 @@ function resetCounter(){
 	document.querySelector('.new-count').className = 'count';
 	counter = 0;
 	document.querySelector('.count-inner').innerHTML = '';
-	document.title = /[0-9]*\ \|\ (.*)/g.exec(document.title) [1];
+	document.title = /[0-9]+\ \|\ (.*)/g.exec(document.title)[1];
 }
 
 /// A "click" event listener attached on the "Notifications" button:
