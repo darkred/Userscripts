b28532b607096313c5907b9effe3140c73aa4451
darkred <rick3162@gmail.com>
2017-03-07 23:12:46 +0200

    Revert "(GreasyFork Bullshit Filter - for TS Citrus Gfork) Changed CSS rule -moz-calc to calc (because -moz-calc doesn't work in Chrome and will also be removed in FF 53)."
    This reverts commit c69f9e09b61ddd2776ffd59fd8ee10170d7f8dfd.

--- Bugzilla_-_reveal_the_Depends,_Blocks,_See_Also_and_Duplicates_bug_titles/Bugzilla_-_reveal_the_Depends,_Blocks,_See_Also_and_Duplicates_bug_titles.user.js
+++ Bugzilla_-_reveal_the_Depends,_Blocks,_See_Also_and_Duplicates_bug_titles/Bugzilla_-_reveal_the_Depends,_Blocks,_See_Also_and_Duplicates_bug_titles.user.js
@@ -3,13 +3,14 @@
 // @namespace   darkred
 // @description Reveal the Depends, Blocks, See Also and Duplicates bug titles in bugzilla.mozilla.org via keyboard shortcuts
 // @include     https://bugzilla.mozilla.org/show_bug.cgi?id=*
-// @version     2017.3.7
+// @version     1.3
 // @grant       none
+// @require		https://code.jquery.com/jquery-3.1.1.min.js
 // @require 	https://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.2/jquery.scrollTo.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/keypress/2.1.3/keypress.min.js
 // ==/UserScript==
 
-/* eslint-disable no-unused-vars */
+
 
 // Case 1: when you press ` (in order to toggle Depends, BLocks and See Also)
 
@@ -23,22 +24,27 @@ var combinedInners = [], seeAlsoInners = [], duplicatesInners = [];
 listener1.simple_combo('`', function() {
 	// console.log('You pressed `');
 
-	depends = $('#field-value-dependson > a');
-	blocks = $('#field-value-blocked > a');
-	seeAlsoRefs = $('#field-value-see_also a');
-	combinedRefs = depends.add(blocks).add(seeAlsoRefs);
+	depends = $('#dependson_input_area  ~ .bz_bug_link');
+	blocks = $('#blocked_input_area ~ .bz_bug_link');
+	combinedRefs = depends.add(blocks);
 
+	seeAlsoRefs = $('.bug_urls > li > a');
 
 	if (flag1 === 1) {
 		flag1 = 0;
 
-		$(window).scrollTo('#field-dependson');
+		$(window).scrollTo('#field_label_see_also');
 
 		$.each(combinedRefs, function(index, val) {	combinedInners[index] = combinedRefs[index].innerHTML;});
+		$.each(seeAlsoRefs, function(index, val) {	seeAlsoInners[index] = seeAlsoRefs[index].innerHTML; });
 
 		$.each(combinedRefs, function(index, val) {
-			combinedRefs[index].nextSibling.remove();
 			combinedRefs[index].innerHTML = '(' + combinedRefs[index].innerHTML + ')  ' + combinedRefs[index].title;
+			combinedRefs[index].outerHTML += '<br/>';
+		});
+
+		$.each(seeAlsoRefs, function(index, val) {
+			seeAlsoRefs[index].innerHTML = '(' + seeAlsoRefs[index].innerHTML + ')  ' + seeAlsoRefs[index].title;
 			combinedRefs[index].outerHTML += '<br/>';
 		});
 
@@ -47,26 +53,22 @@ listener1.simple_combo('`', function() {
 		if (flag1 === 0) {
 			flag1 = 1;
 
-			$.each(combinedRefs, function(index, val) {
-				combinedRefs[index].innerHTML = combinedInners[index];
-				combinedRefs[index].outerHTML += ', ';
-			});
+			$.each(combinedRefs, function(index, val) {	combinedRefs[index].innerHTML = combinedInners[index]; });
 
-			var dependsNL = $('#field-value-dependson > a ~ br');
-			var blocksNL = $('#field-value-blocked > a ~ br');
+			var dependsNL = $('#dependson_input_area  ~ .bz_bug_link ~ br');
+			var blocksNL = $('#blocked_input_area ~ .bz_bug_link  ~ br');
 
 			var combinedNL = dependsNL.add(blocksNL);
 			for (let m = (combinedNL.length) - 1; m >= 0; m -= 1) {
 				combinedNL[m].remove();
 			}
 
+			$.each(seeAlsoRefs, function(index, val) {	seeAlsoRefs[index].innerHTML = seeAlsoInners[index]; });
 		}
-		document.body.scrollTop = document.documentElement.scrollTop = 0;           // scroll to the top of the page
+		// document.body.scrollTop = document.documentElement.scrollTop = 0;           // scroll to the top of the page
 		// window.scrollTo(0, 0);
 		// document.querySelector('html').scrollIntoView();
-
-		// $(window).scrollTo('#field-see_also');
-		$(window).scrollTo('#field-dependson');  // alternative to line 78
+		$(window).scrollTo('#field_label_see_also');
 	}
 });
 
@@ -81,18 +83,16 @@ var flag2 = 1;
 listener2.simple_combo('~', function() {
 	// console.log('You pressed ~');
 
-	duplicatesRefs = $(`a:contains('Duplicates')`).parent().parent().find('.value > a');
+	duplicatesRefs = $('#duplicates > .bz_bug_link');
 
 	if (flag2 === 1) {
 		flag2 = 0;
 
-		// $(window).scrollTo('#duplicates');
-		$(window).scrollTo(`a:contains('Duplicates')`).parent().parent();
+		$(window).scrollTo('#duplicates');
 
 		$.each(duplicatesRefs, function(index, val) {	duplicatesInners[index] = duplicatesRefs[index].innerHTML;});
 
 		$.each(duplicatesRefs, function(index, val) {
-			duplicatesRefs[index].nextSibling.remove();
 			duplicatesRefs[index].innerHTML = '(' + duplicatesRefs[index].innerHTML + ')  ' + duplicatesRefs[index].title;
 			duplicatesRefs[index].outerHTML += '<br/>';
 		});
@@ -101,23 +101,17 @@ listener2.simple_combo('~', function() {
 		if (flag2 === 0) {
 			flag2 = 1;
 
-			$.each(duplicatesRefs, function(index, val) {
-				duplicatesRefs[index].innerHTML = duplicatesInners[index];
-				duplicatesRefs[index].outerHTML += ', ';
-			});
+			$.each(duplicatesRefs, function(index, val) {	duplicatesRefs[index].innerHTML = duplicatesInners[index]; });
 
-
-			var duplicatesNL = $(`a:contains('Duplicates')`).parent().parent().find('.value > a ~ br');
+			var duplicatesNL = $('#duplicates > .bz_bug_link ~ br');
 			for (let k = (duplicatesNL.length) - 1; k >= 0; k -= 1) {
 				duplicatesNL[k].remove();
 			}
-
 		}
 
 		// document.body.scrollTop = document.documentElement.scrollTop = 0;           // scroll to the top of the page
-		window.scrollTo(0, 0);
+		// window.scrollTo(0, 0);
 		// document.querySelector('html').scrollIntoView();
-		// $(window).scrollTo('#duplicates');
-		$(window).scrollTo(`a:contains('Duplicates')`).parent().parent();
+		$(window).scrollTo('#duplicates');
 	}
 });
