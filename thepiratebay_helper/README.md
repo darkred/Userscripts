837450a236355cf99509d42cd1d610b290f605a4
darkred <rick3162@gmail.com>
2017-03-31 02:02:11 +0300

    (thepiratebay helper) Removed the (initial, by emptyparad0x) the custom date handlers, the time format and parsing functions (all the above are now handled by moment.js), the non-appplied code  part that would change the time in the footer, and the no longer relevant options in GM_config (timezone text, use12hour, am/pm and showYearOnTorrentPage) Also, now it also changes the timestamps in torrent pages (incl. in comments) Minor refactoring in general.

--- thepiratebay_helper/README.md
+++ thepiratebay_helper/README.md
@@ -6,7 +6,7 @@ Additional features:
   - hiding non trusted, or 
   - show all
 - Added a keyboard shortcut \` to toggle highlight/hide,
-- Added an option to display torrent timestamps in relative format _(enabled by default)_,
+- Added an option to display torrent timestamps in relative format (and recalculates them for browse/search lists every 10 secs) _(enabled by default)_,
 - Added an option to add a sortable 'Ratio' (seeds/peers) column _(disabled by default)_. 
 
 You may also click on the e.g, `7/30 torrents are currently hidden) - click to toggle`  
@@ -19,7 +19,7 @@ to toggle highlight/hide *(and this is reflected in GM_config too)*.
 
 
 <u>Screenshots</u>:  
-[![](https://i.imgur.com/UjfPtJis.jpg)](https://i.imgur.com/UjfPtJi.jpg) [![](https://i.imgur.com/sTLd6rjs.jpg)](https://i.imgur.com/sTLd6rj.jpg) [![](https://s3.amazonaws.com/uso_ss/21237/thumb.jpg?1366305203)](http://s3.amazonaws.com/uso_ss/21237/large.jpg?1366305203) [![](https://i.imgur.com/lAJiCJcs.jpg)](https://i.imgur.com/lAJiCJc.jpg) [![](https://i.imgur.com/wyyJiujs.jpg)](https://i.imgur.com/wyyJiuj.jpg)
+[![](https://i.imgur.com/UjfPtJis.jpg)](https://i.imgur.com/UjfPtJi.jpg) [![](https://i.imgur.com/LRIXDwys.jpg)](https://i.imgur.com/LRIXDwy.jpg) [![](https://s3.amazonaws.com/uso_ss/21237/thumb.jpg?1366305203)](http://s3.amazonaws.com/uso_ss/21237/large.jpg?1366305203) [![](https://i.imgur.com/lAJiCJcs.jpg)](https://i.imgur.com/lAJiCJc.jpg) [![](https://i.imgur.com/wyyJiujs.jpg)](https://i.imgur.com/wyyJiuj.jpg)
 
 <br/>
 
@@ -37,15 +37,12 @@ Thanks to emptyparad0x for making a very useful script!
 
 <u><b>Info for the script in http://userscripts-mirror.org/scripts/show/164849</b></u>
 
-- This script changes the times everywhere on thepiratebay except for the comments. It changes the time shown in the browse/search lists, the footer, and the torrent pages themselves.
+- This script changes the times everywhere on thepiratebay except for the comments. It changes the time shown in the browse/search lists and the torrent pages themselves (including the comments).
 - At the bottom of the page, you will find the configuration menu for the script. This is where you will enter how you'd like your times displayed.
-- The `Text for Timezone` field is what will be displayed after the time. This defaults to EST, but can be changed to whatever you like. It can also be left blank.
-- The `Use AM/PM` checkbox determines if your times will be displayed in a 12 hour format.
-- The `Text for AM/PM` fields are how the script will denote AM/PM. If you'd like a space between the time and the denotation, enter a space before the AM or PM in these fields (e.g. " am" without the quotes).
 - The `TPB Timezone` allows for you to adjust the timezone that thepiratebay is currently displaying. As of 4/18/2013 the timezone is GMT+1, therefore its value should be left as it is, i.e. `(GMT+1) + 0`.
 - After saving settings, the script will reload the page.
 
-**Tech Info**
+**Tech Info**  
 This script uses GM_config and jQuery. It has not been tested for conflicts with other userscripts or languages besides English. It has been tested with GreaseMonnkey on Firefox and Tampermonkey on Chrome.
 <br>
-*It uses [Keypress](https://github.com/dmauro/Keypress/) keyboard input capturing utility.*
+*It uses [Keypress](https://github.com/dmauro/Keypress/) keyboard input capturing utility and the jQuery plugin [tablesorter](http://mottie.github.io/tablesorter/docs/index.html) (forked by Rob Garrison (Mottie)) .*
--- thepiratebay_helper/thepiratebay_helper.user.js
+++ thepiratebay_helper/thepiratebay_helper.user.js
@@ -1,10 +1,10 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        thepiratebay helper
 // @namespace   darkred
 // @authors     emptyparad0x, darkred
 // @description Converts dates to local timezone on thepiratebay and optionally either highlight VIP/Trusted/Moderator/Helper torrents or hide non verified torrents altogether
-// @version     0.9.6g
-// @date        2017.3.14
+// @version     0.9.6h
+// @date        2017.3.31
 // @include     /^https?://thepiratebay\.(org|se|gd|la|mn|vg)/search.*$/
 // @include     /^https?://thepiratebay\.(org|se|gd|la|mn|vg)/browse/.*$/
 // @include     /^https?://thepiratebay\.(org|se|gd|la|mn|vg)/user/.*$/
@@ -14,7 +14,9 @@
 // @include     /^https?://thepiratebay\.(org|se|gd|la|mn|vg)/music.*$/
 // @include     /^https?://thepiratebay\.(org|se|gd|la|mn|vg)/top.*$/
 // @grant       none
-// @require     https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @run-at      document-idle
+// @require     https://code.jquery.com/jquery-3.2.0.min.js
+// @require     https://greasyfork.org/scripts/28536-gm-config/code/GM_config.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/keypress/2.1.4/keypress.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.28.5/js/jquery.tablesorter.min.js
@@ -23,33 +25,52 @@
 /* global $:false, GM_config, moment */
 
 
+
+
+// Customize the strings in the locale to display "1 minute ago" instead of "a minute ago" (https://github.com/moment/moment/issues/3764#issuecomment-279928245)
+moment.updateLocale('en', {
+	relativeTime: {
+		future: 'in %s',
+		past:   '%s ago',
+		s:  'seconds',
+		m:  '1 minute',
+		mm: '%d minutes',
+		h:  '1 hour',
+		hh: '%d hours',
+		d:  '1 day',
+		dd: '%d days',
+		M:  '1 month',
+		MM: '%d months',
+		y:  '1 year',
+		yy: '%d years'
+	}
+});
+
+
+
+
+
 $('table td').css({ 'white-space': 'nowrap' });		// In order to prevent the text in all table cells from wrapping
 $('#main-content').css({ 'overflow': 'visible' });	// Override the default rule which is 'hidden' ('#main-content' is the parent of the search results' 'table' element)
 
 
+
 //GM_config stuff
 GM_config.init('TPB Helper settings',{
-	timezone: {label: 'Text for Timezone:', type: 'text', cols: 10, default: 'EST'},
-	use12hour: {label: 'Use AM/PM:', type: 'checkbox', default: false},
-	am:	{label: 'Text for AM:', type: 'text', cols: 10, default: 'am'},
-	pm:	{label: 'Text for PM:', type: 'text', cols: 10, default: 'pm'},
+	// timezone: {label: 'Text for Timezone:', type: 'text', cols: 10, default: 'EST'},
 	tpboffset: {label: 'TPB Timezone offset    : (GMT+1)  +', type: 'int', default: 0},             // Initially it was:   tpboffset:    { label: 'TPB Timezone: GMT+', type: 'int', default: 1 },
-	showYearOnTorrentPage: {label: 'Display Year On Torrent Page:', type: 'checkbox', default: false},
-	enhanceVisibility: {label: 'Show all / Highlight trusted / Hide non-trusted:', section: ['Extras'], type: 'select', options: ['Show all', 'Highlight','Hide'], default: 'Show all'},
+	enhanceVisibility: {label: 'Show all / Highlight trusted / Hide non-trusted:',	section: ['Extras'], type: 'select', options: ['Show all', 'Highlight','Hide'], default: 'Show all'},
 	relativeDates: {label: 'Display torrent timestamps in relative format:', type: 'checkbox', default: true},
 	sortableRtColumn: {label: 'Add a sortable Ratio column?', type: 'checkbox', default: false}
 },{
-	save: function(){location.reload();}});
+	save: function(){location.reload();}
+});
 
 $('body').append(`<center><a id='TimeChangerConfig'>TPB Helper settings</a></center>`);
 $('#TimeChangerConfig').click(function(){GM_config.open();}).css({'cursor': 'pointer'});
 
-var timezone = GM_config.get('timezone');
+// var timezone = GM_config.get('timezone');
 var tpboffset = GM_config.get('tpboffset');
-var use12hour = GM_config.get('use12hour');
-var am = GM_config.get('am');
-var pm = GM_config.get('pm');
-var showYearOnTorrentPage = GM_config.get('showYearOnTorrentPage');
 var enhanceVisibility = GM_config.get('enhanceVisibility');
 var relativeDates = GM_config.get('relativeDates');
 var sortableRtColumn = GM_config.get('sortableRtColumn');
@@ -62,7 +83,9 @@ var sortableRtColumn = GM_config.get('sortableRtColumn');
 // For the sortable Ratio column
 function appendColumn() {
 	var se, le, ratio;
-	document.querySelector('.header > th:nth-child(7)').insertAdjacentHTML('afterend', '<th><abbr title="Seeders">Rt</abbr></th>');
+	if (document.querySelector('.header > th:nth-child(7)')){
+		document.querySelector('.header > th:nth-child(7)').insertAdjacentHTML('afterend', '<th><abbr title="Seeders">Rt</abbr></th>');
+	}
 	var entries = document.querySelectorAll('#searchResult > tbody:nth-child(2) > tr > td:nth-child(7)');
 	for (var i = 0; i < entries.length; i++) {
 		se = parseInt(entries[i].previousElementSibling.innerHTML);   // Retrieve the content of the cell of the SE column and store it to variable se
@@ -103,231 +126,18 @@ if (sortableRtColumn === true) {
 
 
 
-
-
-
-
-//Custom Date Handlers
-Date.prototype.addHours = function(h) {
-	this.setTime(this.getTime() + (h*60*60*1000));
-	return this;
-};
-Date.prototype.stdTimezoneOffset = function() {
-	var jan = new Date(this.getFullYear(), 0, 1);
-	var jul = new Date(this.getFullYear(), 6, 1);
-	return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
-};
-Date.prototype.dst = function() {
-	return this.getTimezoneOffset() < this.stdTimezoneOffset();
-};
-
-var d = new Date();
-var tpbTimezoneAdder = (tpboffset * 60) + d.getTimezoneOffset();
-
-if (d.dst){
-	tpbTimezoneAdder += 60;
-}
-
-var hours = (tpbTimezoneAdder / 60);
-
-var tpbTodayDate = new Date();
-
-tpbTodayDate.addHours(hours);
-
-var tpbTodayYear = tpbTodayDate.getFullYear();
-var tpbTodayMonth = tpbTodayDate.getMonth();
-var tpbTodayDay = tpbTodayDate.getDate();
-
-var tpbYdayDate = new Date(tpbTodayDate);
-tpbYdayDate.addHours(-24);
-
-var tpbYdayYear = tpbYdayDate.getFullYear();
-var tpbYdayMonth = tpbYdayDate.getMonth();
-var tpbYdayDay = tpbYdayDate.getDate();
-
-var yesterday = new Date();
-yesterday.addHours(-24);
-
-var torYear, torMonth, torDate, torHour, torMinute;
-var dispTime = true;
-
-// Time Parsing Function For Search/Browse pages
-function parseTime(str){
-	torYear = null;
-	torMonth = null;
-	torDate = null;
-	torHour = null;
-	torMinute = null;
-	dispTime = true;
-
-	if (str.indexOf('Today') > -1) {
-		// Format is in 'Today hh:mm'
-		torYear = tpbTodayYear;
-		torMonth = tpbTodayMonth;
-		torDate = tpbTodayDay;
-		torHour = str.slice(11,13);
-		torMinute = str.slice(14,16);
-	}
-
-	else if (str.indexOf('Y-day') > -1){
-		// Format is in 'Y-day hh:mm'
-		torYear = tpbYdayYear;
-		torMonth = tpbYdayMonth;
-		torDate = tpbYdayDay;
-		torHour = str.slice(11,13);
-		torMinute = str.slice(14,16);
-	}
-
-	else if ((str.indexOf('-') > -1) && (str.indexOf(':') > -1) && (str.length == 16)){
-		// Format is in 'MM-DD hh:mm'
-		torYear = tpbTodayYear;
-		torMonth = str.slice(0,2)-1;
-		torDate = str.slice(3,5);
-		torHour = str.slice(11,13);
-		torMinute = str.slice(14,16);
-	}
-	else if ((str.indexOf('-') > -1) && (str.length == 15)){
-		// Format is in 'MM-DD YYYY'
-		torYear = str.slice(11,15);
-		torMonth = str.slice(0,2)-1;
-		torDate = str.slice(3,5);
-		torHour = hours;
-		torMinute = '00';
-		dispTime = false;
-	}
-
-	var torFullDate = new Date(torYear, torMonth, torDate, torHour, torMinute);
-
-	torFullDate.addHours(-hours);
-	var output = formatTime(torFullDate);
-	return output;
-}
-// End Time Parsing Function For Search/Browse pages
-
-// Time Format Function
-function formatTime(tempDate, time_override, year_override){
-	var t_output;
-	if (tempDate.toDateString()==d.toDateString()){
-		t_output = 'Today ';
-	}
-	else if (tempDate.toDateString()==yesterday.toDateString()){
-		t_output = 'Y-day ';
-	}
-	else {
-		t_output = ('0' + (tempDate.getMonth()+1)).slice(-2) +
-			'-' + ('0' + tempDate.getDate()).slice(-2);
-
-		if ((tempDate.getFullYear() != tpbTodayYear) || (year_override)){
-			t_output = t_output + '-' + tempDate.getFullYear();
-		}
-
-		t_output += ' ';
-	}
-
-	if (dispTime || time_override){
-		if (use12hour){
-			var ampm;
-			var temph;
-			if (tempDate.getHours()>=12){
-				ampm = pm;
-				temph = (tempDate.getHours()-12);
-			}
-			else {
-				ampm = am;
-				temph = (tempDate.getHours());
-			}
-			if (temph===0){
-				temph = 12;
-			}
-			tempDate.setHours(temph);
-			t_output = t_output + tempDate.getHours() + ':' + ('0' + tempDate.getMinutes()).slice(-2) + ampm;
-		}
-		else {
-			t_output = t_output + ('0' + tempDate.getHours()).slice(-2) + ':' + ('0' + tempDate.getMinutes()).slice(-2);
-		}
-		t_output = t_output + ' ' + timezone;
-		t_output = t_output.trim();			// remove trailing spaces
-	}
-	return t_output;
-}
-// End Time Format Function
-
-// Time Parsing Function for torrent pages
-function parseTimeString(str){
-	var torYear =  str.slice(0,4);
-	var torMonth =  str.slice(5,7)-1;
-	var torDate = str.slice(8,10);
-	var torHour = str.slice(11,13);
-	var torMinute = str.slice(14,16);
-	var torSecond = str.slice(17,19);
-	var FullDate = new Date(torYear, torMonth, torDate, torHour, torMinute, torSecond);
-	return FullDate;
-}
-// End Time Parsing Function for torrent pages
-
-//Change the time on the footer
-$('p#footer, div#stats').each(function(){
-	var f_str = $(this).html();
-	// var f_strTime;
-	var f_pos = f_str.indexOf('Last updated');
-	if (f_pos > -1){
-		f_pos += 13;
-		var f_startString = f_str.slice(0,f_pos);
-		var f_strTime = f_str.slice(f_pos,(f_pos+8));
-		var f_endString = f_str.slice((f_pos+9),f_str.length);
-		var f_footerDate = new Date();
-		f_footerDate.setHours(f_strTime.slice(0,2));
-		f_footerDate.setMinutes(f_strTime.slice(3,5));
-		f_footerDate.setSeconds(f_strTime.slice(6,8));
-		f_footerDate.addHours(-hours);
-		f_strTime = formatTime(f_footerDate,true);
-		$(this).html(f_startString + f_strTime + f_endString);
-	}
-
-});
-
 //Check page
 var url = window.location.href;
 var host = window.location.host;
-if (url.indexOf(host + '/torrent/') != -1) {
-	//Change time on the torrent page
-	var torPageFullDate = parseTimeString($('dt:contains(\'Uploaded\')').next().html());
-	torPageFullDate.addHours(-hours);
-	$('dt:contains(\'Uploaded\')').next().html(formatTime(torPageFullDate,true, showYearOnTorrentPage));
-	jQuery.noConflict(); //Need this to run last in order to fix problem with thepiratebay comments
+if (url.indexOf(host + '/torrent/') !== -1) {
+	if (relativeDates === true) {
+		convertDatesInTorrentPage();
+	}
 } else {
-	// Change the times on the browse/search pages
-	var allBody = document.body.innerHTML;
-	if (allBody.indexOf('view=s') > -1){
-		// Page is in Double View
-		$('table#searchResult font').each(function(){
-			var str = $(this).html();
-			if (str.indexOf('Uploaded') > -1){
-				var c_loc = str.indexOf(',');
-				var strTime = str.slice(9,c_loc);
-				if (strTime.indexOf('min')==-1) {
-					//Do nothing with times displayed in "n Mins ago"
-					var strLast = str.slice(c_loc, str.length);
-					$(this).html('Uploaded ' + parseTime(strTime) + strLast);
-				}
-			}
-		});
-	}
-	else if (allBody.indexOf('view=d') > -1){
-		// Page is in Single View
-		$('table#searchResult td:nth-child(3)').each(function(){
-			if ($(this).html().indexOf('min')==-1){
-			//Do nothing with times displayed in "n Mins ago"
-				$(this).html(parseTime($(this).html()));
-			}
-		});
-	}
-
-
 
 
 	// Enhance trusted torrents
-	if (enhanceVisibility == 'Highlight') {
+	if (enhanceVisibility === 'Highlight') {
 		highlight();
 	}
 
@@ -335,43 +145,23 @@ if (url.indexOf(host + '/torrent/') != -1) {
 
 	// Hide non trusted torrents
 	var counter = 0;
-	if (enhanceVisibility == 'Hide') {
+	if (enhanceVisibility === 'Hide') {
 
 		hideNonTrusted();
-//     alert (counter);
+		//     alert (counter);
 		if (counter > 0) {
 			document.querySelector('#TimeChangerConfig').insertAdjacentHTML('afterend', '<br><i>' + counter  + ' non verified torrents hidden - click here or press ` to view all torrents</i>');
 		}
 		var flagHide = true;
 
 
-
-		function toggleHide() {
-			if (counter > 0) {
-				if (flagHide == true) {
-					// $('table#searchResult tbody tr').each(function() {
-					$(getAllTableLines()).each(function() {
-						$(this).children('td').show();
-					});
-					$('#TimeChangerConfig').nextAll().eq(1).html('<i>click here (or press `) to view only verified torrents</i>');
-					flagHide = false;
-				} else {
-					counter = 0;
-					hideNonTrusted();
-					$('#TimeChangerConfig').nextAll().eq(1).html('<i>' + counter  + ' non verified torrents hidden - click here or press ` to view all torrents</i>');
-					flagHide = true;
-				}
-			}
-
-		}
-
-
-
 		$('#TimeChangerConfig').nextAll().eq(1).on('click', toggleHide);
+		// $('#TimeChangerConfig').nextAll().eq(1).on('click', hideNonTrusted);
 
 		var listener2 = new window.keypress.Listener();
 		listener2.simple_combo('`', function() {
 			toggleHide();
+			// hideNonTrusted();
 		});
 
 
@@ -381,6 +171,25 @@ if (url.indexOf(host + '/torrent/') != -1) {
 
 
 
+
+}
+
+
+function toggleHide() {
+	if (counter > 0) {
+		if (flagHide === true) {
+			$(getAllTableLines()).each(function() {
+				$(this).children('td').show();
+			});
+			$('#TimeChangerConfig').nextAll().eq(1).html('<i>click here (or press `) to view only verified torrents</i>');
+			flagHide = false;
+		} else {
+			counter = 0;
+			hideNonTrusted();
+			$('#TimeChangerConfig').nextAll().eq(1).html('<i>' + counter  + ' non verified torrents hidden - click here or press ` to view all torrents</i>');
+			flagHide = true;
+		}
+	}
 
 }
 
@@ -404,11 +213,7 @@ function getAllTableLines(){
 
 
 function hideNonTrusted() {
-	// var total = 0;
-	// var counter = 0;
-	// $('table#searchResult tbody tr').each(function() {
 	$(getAllTableLines()).each(function() {
-		// total++;
 		if (($(this).html().indexOf('title="VIP"') > -1) ||
 			($(this).html().indexOf('title="Trusted"') > -1) ||
 			($(this).html().indexOf('title="Moderator"') > -1) ||
@@ -416,7 +221,8 @@ function hideNonTrusted() {
 			($(this).html().indexOf('src="https://piratebay.org/img/next.gif"') > -1)) {
 			//do nothing
 		} else {
-			$(this).children('td').hide();
+			// $(this).children('td').hide();
+			$(this).children('td').toggle();
 			counter++;
 		}
 	});
@@ -460,59 +266,102 @@ function highlight() {
 
 }
 
-// Customize the strings in the locale to display "1 minute ago" instead of "a minute ago" (https://github.com/moment/moment/issues/3764#issuecomment-279928245)
-moment.updateLocale('en', {
-	relativeTime: {
-		future: 'in %s',
-		past:   '%s ago',
-		s:  'seconds',
-		m:  '1 minute',
-		mm: '%d minutes',
-		h:  '1 hour',
-		hh: '%d hours',
-		d:  '1 day',
-		dd: '%d days',
-		M:  '1 month',
-		MM: '%d months',
-		y:  '1 year',
-		yy: '%d years'
-	}
-});
 
 
-function convertDates(times) {
+function convertDates() {
+	// alert();
 	var dates = document.querySelectorAll('#searchResult > TBODY > TR > TD:nth-child(3)');
 	for (var i = 0; i < dates.length; i++) {
-		if (times === 1 ) {						// if it's the 1st time the function is called
-			var initial = dates[i].innerHTML;
-		} else {								// else (in consequent calls)
+		if (dates[i].title === '' ) {		// if it's the 1st time the function is called
+			var initial = dates[i].innerHTML.replace(/&nbsp;/g, ' ').replace(/<\/?b>/g, '');
+		} else {												// else (in consequent calls)
 			initial = dates[i].title;
 		}
-		var temp = initial.replace(timezone, '');	// remove timezone
-		temp = temp.trim();							// remove trailing spaces
-		if (temp.indexOf('Today') !== -1) {
-			temp = temp.replace('Today', moment().format('MM-DD'));
+		var temp = initial.trim();							// remove trailing spaces
+		// 50 mins ago
+		// Note: randomly, if the timestamps in browse/search lists are with in the 59 last mins (i.e. < 1 hour),
+		// then they are displayed in relative format (in bold) i.e. already in GMT+1
+		// instead of like this e.g. "Today 22:10", i.e. GMT, therefore it needs to be converted to GMT+1
+		var relativeTimestampByThePage = false;
+		if (temp.indexOf('mins ago') !== -1 || temp.indexOf('min ago') !== -1) {
+			// temp = moment().subtract(parseInt(temp), 'minutes').format('MM-DD-YYYY HH:mm');
+			// temp = moment(temp, 'MM-DD-YYYY HH:mm');
+			temp = moment().subtract(parseInt(temp), 'minutes');
+			relativeTimestampByThePage = true;
+		} else if (temp.indexOf('Today') !== -1) {
+			// Today 22:10
+			temp = temp.replace('Today', moment().format('MM-DD-YYYY'));
+			temp = moment(temp, 'MM-DD-YYYY HH:mm');
+
 		} else if (temp.indexOf('Y-day') !== -1) {
-			temp = temp.replace('Y-day', moment().subtract(1, 'days').format('MM-DD'));
+			// Y-day 22:02
+			temp = temp.replace('Y-day', moment().subtract(1, 'days').format('MM-DD-YYYY'));
+			temp = moment(temp, 'MM-DD-YYYY HH:mm');
+
+		} else if (moment(temp, 'MM-DD HH:mm', true).isValid()) {
+			// 03-24 07:32
+			temp = moment(temp, 'MM-DD HH:mm');
+
+		} else if (moment(temp, 'MM-DD YYYY', true).isValid()) {
+			// 12-18 2016
+			temp = moment(temp, 'MM-DD YYYY');
+
+		} else {
+			temp = moment(temp, 'MM-DD-YYYY HH:mm', true);
 		}
-		if (moment(temp, 'MM-DD HH:mm', true).isValid()) {
-			dates[i].innerHTML = moment(temp, 'MM-DD HH:mm').fromNow();
-			dates[i].title = initial;
-		} else if (moment(temp, 'MM-DD-YYYY', true).isValid()) {
-			dates[i].innerHTML = moment(temp, 'MM-DD-YYYY').fromNow();
-			dates[i].title = initial;
+
+		// if the displayed timestamp (from the page itself) is in relative format
+		if (relativeTimestampByThePage !== true && dates[i].getAttribute('offset') !== 'true'){
+			temp.add(tpboffset + 1, 'hours');			// then add the offset (+1 hour) to 'temp'
+			dates[i].setAttribute('offset', true);
 		}
+
+
+
+		// if the produced moment 'temp' object is 22 hours  later than now
+		if (moment().diff(temp, 'hour') === -22 && dates[i].getAttribute('minus1day') !== 'true'){
+			temp.subtract(1, 'days');					// then subtract 1 day from that
+			dates[i].setAttribute('minus1day', true);
+		}
+
+
+		dates[i].innerHTML = temp.fromNow();
+		dates[i].title = temp.format('MM-DD-YYYY HH:mm');
+		// dates[i].title = initial;
+
+
 	}
 }
 
 if (relativeDates === true) {
-	convertDates(1);
+	// convertDates();
 
 	// recalculate the timestamps in relative format every 10 sec
 	(function(){
-		convertDates(2);
-		setTimeout(arguments.callee, 1 * 10 * 1000);
+		convertDates();
+		// setTimeout(arguments.callee, 1 * 10 * 1000);
+		setTimeout(arguments.callee, 1 * 2 * 1000);
 	})();
 
 
+}
+
+function convertDatesInTorrentPage(){
+	// FOR THE TORRENT TIMESTAMP
+	// example: 2017-03-29 17:54:56 GMT
+	var torrentTimestamp = document.querySelector('.col2 > dd:nth-child(2)');
+	var initial = torrentTimestamp.innerHTML;
+	torrentTimestamp.innerHTML = moment(initial, 'YYYY-MM-DD HH:mm:ss').add(tpboffset + 1, 'hour').fromNow();
+	torrentTimestamp.title = initial;
+
+	// FOR THE COMMENT TIMESTAMPS
+	var dates = document.querySelectorAll('#comments > div[id^="comment-"] > p');
+	for (var i = 0; i < dates.length; i++) {
+		// example: 2016-11-23 17:53 CET
+		var currentElement = dates[i].childNodes[2];
+		initial = currentElement.nodeValue.trim().replace('at ', '').slice(0, -1);
+		currentElement.nodeValue = ' ' + moment(initial, 'YYYY-MM-DD HH:mm zz').add(tpboffset + 1, 'hour').fromNow();
+		currentElement.parentElement.title = initial;
+
+	}
 }
