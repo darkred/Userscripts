860d6f29e460d77cb4a273deea1d95041a2c0753
darkred <rick3162@gmail.com>
2017-04-29 01:20:50 +0300

    (RARBG - torrent and magnet links) Removed a pointless 'if', renamed some variabled, and added more comments

--- RARBG_-_torrent_and_magnet_links/RARBG_-_torrent_and_magnet_links.user.js
+++ RARBG_-_torrent_and_magnet_links/RARBG_-_torrent_and_magnet_links.user.js
@@ -3,52 +3,54 @@
 // @namespace   darkred
 // @description Adds a column with torrent and magnet links in RARBG lists
 // @include     /^(https?:)?\/\/(www\.)?rarbg\.(to|com)\/(torrents\.php.*|catalog\/.*|top10)$/
-// @version     2017.02.16
+// @version     2017.4.27
 // @grant       none
 // ==/UserScript==
 
 
 
 function appendColumn(title) {
-	if (document.querySelectorAll('.lista2t > tbody > tr').length !== 0) {
 
-		var entries = document.querySelectorAll('.lista2t > tbody > tr > td:nth-child(2) ');
-		for (let i = 0; i < entries.length; i++) {
-			entries[i].insertAdjacentHTML('afterend', '<td>link</td>');
+	var entries = document.querySelectorAll('.lista2t > tbody > tr > td:nth-child(2) ');		// the initial column 'Files' after of which the extra column will be appended
+
+	for (let i = 0; i < entries.length; i++) {													// creation of the extra column
+		entries[i].insertAdjacentHTML('afterend', `<td>` + title + `</td>`);
+	}
+
+	var header = document.querySelector('.lista2t > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)');		// the first cell (the header cell) of the new column
+	header.innerHTML = title;
+	header.setAttribute('class', 'header6');
+	header.setAttribute('align', 'center');
+
+	var cells = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');				// the rest cells of the new column
+	for (let i = 0; i < cells.length; i++) {
+		cells[i].setAttribute('class', 'lista');
+		cells[i].setAttribute('width', '50px');
+		cells[i].setAttribute('align', 'center');
+	}
+
+	var newColumn = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');		// new column
+	var oldColumn = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(2)');		// old column
+
+
+	// populate the cells in the new column with DL and ML links
+	for (let i = 0; i < newColumn.length; i++) {
+		if ((/over\/(.*)\.jpg\\/).test(oldColumn[i].firstChild.outerHTML)){
+			var hash = oldColumn[i].firstChild.outerHTML.match(/over\/(.*)\.jpg\\/)[1];
+		} else {
+			hash = undefined;
 		}
+		let title = oldColumn[i].firstChild.innerText;
+		var trackers = 'http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710&tr=udp%3A%2F%2F9.rarbg.to%3A2710';
 
-		var header = document.querySelector('.lista2t > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)');
-		header.innerHTML = title;
-		header.setAttribute('class', 'header6');
-		header.setAttribute('align', 'center');
-		var cells = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');
-		for (let i = 0; i < cells.length; i++) {
-			cells[i].setAttribute('class', 'lista');
-			cells[i].setAttribute('width', '50px');
-			cells[i].setAttribute('align', 'center');
-		}
-
-		var arr1 = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');		// new column
-		var arr2 = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(2)');		// old column
-
-
-		for (let i = 0; i < arr1.length; i++) {
-			if ((/over\/(.*)\.jpg\\/).test(arr2[i].firstChild.outerHTML)){
-				var hash = arr2[i].firstChild.outerHTML.match(/over\/(.*)\.jpg\\/)[1];
-			} else {
-				hash = undefined;
-			}
-			let title = arr2[i].firstChild.innerText;
-			var trackers = 'http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710&tr=udp%3A%2F%2F9.rarbg.to%3A2710';
-
-			arr1[i].innerHTML = '<a href="' + arr2[i].firstChild.href.replace('torrent/', 'download.php?id=') + '&f=' + arr2[i].firstChild.innerText + '-[rarbg.com].torrent"><img src="https://dyncdn.me/static/20/img/16x16/download.png"">' + '</>';
-			if (hash !== undefined){
-				arr1[i].innerHTML += '&nbsp;<a href="magnet:?xt=urn:btih:' + hash + '&dn=' + title + '&tr=' + trackers + ' "><img src="https://dyncdn.me/static/20/img/magnet.gif""></>';
-			} else {
-				arr1[i].innerHTML += '&nbsp;&nbsp;&nbsp;&nbsp;';
-			}
+		newColumn[i].innerHTML = '<a href="' + oldColumn[i].firstChild.href.replace('torrent/', 'download.php?id=') + '&f=' + oldColumn[i].firstChild.innerText + '-[rarbg.com].torrent"><img src="https://dyncdn.me/static/20/img/16x16/download.png"">' + '</>';
+		if (hash !== undefined){
+			newColumn[i].innerHTML += '&nbsp;<a href="magnet:?xt=urn:btih:' + hash + '&dn=' + title + '&tr=' + trackers + ' "><img src="https://dyncdn.me/static/20/img/magnet.gif""></>';
+		} else {
+			newColumn[i].innerHTML += '&nbsp;&nbsp;&nbsp;&nbsp;';
 		}
 	}
+
 }
 
 appendColumn('DL&nbsp;ML');
