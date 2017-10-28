e9de25d1d5a804afd9e82bdf81e4839f3d44de70
darkred <rick3162@gmail.com>
2017-10-28 22:29:45 +0300

    [RARBG - torrent and magnet links] Now it displays ML for all torrent entries: it gets the magnet link by retrieving the target torrent page via xhr. (Thanks to sxe for the suggestion)

--- RARBG_-_torrent_and_magnet_links/RARBG_-_torrent_and_magnet_links.user.js
+++ RARBG_-_torrent_and_magnet_links/RARBG_-_torrent_and_magnet_links.user.js
@@ -3,35 +3,33 @@
 // @namespace   darkred
 // @description Adds a column with torrent and magnet links in RARBG lists
 // @include     /^(https?:)?\/\/(www\.)?rarbg\.(to|com)\/(torrents\.php.*|catalog\/.*|top10)$/
-// @version     2017.4.27
+// @version     2017.10.28
 // @grant       none
 // ==/UserScript==
 
 
-
 function appendColumn(title) {
 
-	var entries = document.querySelectorAll('.lista2t > tbody > tr > td:nth-child(2) ');		// the initial column 'Files' after of which the extra column will be appended
+	var entries = document.querySelectorAll('.lista2t > tbody > tr > td:nth-child(2) ');        // the initial column 'Files' after of which the extra column will be appended
 
-	for (let i = 0; i < entries.length; i++) {													// creation of the extra column
+	for (let i = 0; i < entries.length; i++) {                                                  // creation of the extra column
 		entries[i].insertAdjacentHTML('afterend', `<td>` + title + `</td>`);
 	}
 
-	var header = document.querySelector('.lista2t > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)');		// the first cell (the header cell) of the new column
+	var header = document.querySelector('.lista2t > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)');       // the first cell (the header cell) of the new column
 	header.innerHTML = title;
 	header.setAttribute('class', 'header6');
 	header.setAttribute('align', 'center');
 
-	var cells = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');				// the rest cells of the new column
+	var cells = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');               // the rest cells of the new column
 	for (let i = 0; i < cells.length; i++) {
 		cells[i].setAttribute('class', 'lista');
 		cells[i].setAttribute('width', '50px');
 		cells[i].setAttribute('align', 'center');
 	}
 
-	var newColumn = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');		// new column
-	var oldColumn = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(2)');		// old column
-
+	var newColumn = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');       // new column
+	var oldColumn = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(2)');       // old column
 
 	// populate the cells in the new column with DL and ML links
 	for (let i = 0; i < newColumn.length; i++) {
@@ -42,15 +40,53 @@ function appendColumn(title) {
 		}
 		let title = oldColumn[i].firstChild.innerText;
 		var trackers = 'http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710&tr=udp%3A%2F%2F9.rarbg.to%3A2710';
-
 		newColumn[i].innerHTML = '<a href="' + oldColumn[i].firstChild.href.replace('torrent/', 'download.php?id=') + '&f=' + oldColumn[i].firstChild.innerText + '-[rarbg.com].torrent"><img src="https://dyncdn.me/static/20/img/16x16/download.png"">' + '</>';
+		// if the torrent hash is contained in the filenames of the thumbnail image
 		if (hash !== undefined){
+			// then generate magnet link from preview thumbnail if available
 			newColumn[i].innerHTML += '&nbsp;<a href="magnet:?xt=urn:btih:' + hash + '&dn=' + title + '&tr=' + trackers + ' "><img src="https://dyncdn.me/static/20/img/magnet.gif""></>';
 		} else {
-			newColumn[i].innerHTML += '&nbsp;&nbsp;&nbsp;&nbsp;';
+			// else generate it via an ajax request
+			let href = oldColumn[i].firstChild.href;
+			newColumn[i].innerHTML += '&nbsp;<a class="xhrMagnetLink" data-href="' + href + '" href="#"><img src="https://dyncdn.me/static/20/img/magnet.gif""></>';
+			newColumn[i].lastChild.title = 'ML generated via an ajax request';
 		}
 	}
-
 }
 
 appendColumn('DL&nbsp;ML');
+
+var xhrMagnetLinks = document.querySelectorAll('.xhrMagnetLink');
+
+for(let i=0; i < xhrMagnetLinks.length; i++) {
+
+	xhrMagnetLinks[i].addEventListener('mouseover', function(event){
+
+		event.preventDefault();
+		let href = this.getAttribute('href');
+		if (href === '#') {
+			let tLink = this.getAttribute('data-href');
+
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', tLink, true);	// XMLHttpRequest.open(method, url, async)
+			xhr.onload = function () {
+
+				let container = document.implementation.createHTMLDocument().documentElement;
+				container.innerHTML = xhr.responseText;
+				let magnetLink = container.querySelector('a[href^="magnet:"]');		// the 'magnet link' element in the retrieved page
+
+				if (magnetLink) {
+					let magnetHref = magnetLink.href;
+					this.href = magnetHref;
+					let currentMagnetLink = document.querySelector('a[data-href^="' + tLink + '"]');	// the current magnet link element
+					currentMagnetLink.setAttribute('href', magnetHref);
+				}
+
+			};
+			xhr.send();
+
+		}
+
+	}, false);
+
+}
--- RARBG_-_torrent_and_magnet_links/README.md
+++ RARBG_-_torrent_and_magnet_links/README.md
@@ -1,11 +1,14 @@
 Adds a column with torrent and magnet links in RARBG lists:  
-![](https://i.imgur.com/qmxoJyi.jpg)
+![](https://i.imgur.com/JpNCgIe.jpg)
 
 Notes: 
 
-- the script generates the magnet links based on the filenames of the thumbnail images 
-that appear when you mouseover on a torrent title  
-(because these filenames are the same as the relevant torrent hash).  
-But for some torrents the thumbnail filename is generic (and not equal to the torrent hash) 
-therefore the magnet link cannot be generated.  
-- Also, in the Catalog section of the site there are not thumbnails, therefore the magnet link there cannot be generated either.  
+- the script generates the magnet links in two ways: 
+  - for most torrent entries, it generates it from the filename of the thumbnail images that appears when you mouseover on the torrent title  
+*(because these filenames are the same as the relevant torrent hash)*.  
+  - For some torrents however, the thumbnail filename is generic (i.e. it doesn't contain the torrent hash), so the magnet link cannot be generated directly.  
+In such cases the target torrent page is retrieved via xhr, in order to get the magnet link:  
+as you mouseover on the ML icon for such entries, initially it will have the current page's URL in the status bar  
+and the following tooltip will be displayed: "`ML generated via an ajax request`".  
+During that time the target page will be retrieved via xhr in the background,  
+and so if you click the ML icon (or just move the mouse away and re-mouseover), it will now have the magnet link.
