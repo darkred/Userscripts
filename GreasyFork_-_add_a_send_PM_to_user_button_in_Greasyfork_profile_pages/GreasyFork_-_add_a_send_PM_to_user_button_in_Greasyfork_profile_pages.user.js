065b0170f801b963ec1ccfba269860ddb49b82ce
darkred <rick3162@gmail.com>
2017-11-04 18:52:20 +0200

    (GreasyFork - add a 'send PM to user' button in Greasyfork profile pages) Now compatible with Greasemonkey 4.0

--- GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages/GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages.user.js
+++ GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages/GreasyFork_-_add_a_send_PM_to_user_button_in_Greasyfork_profile_pages.user.js
@@ -1,33 +1,37 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        GreasyFork - add a 'send PM to user' button in Greasyfork profile pages
 // @namespace   darkred
 // @description It adds a 'send PM to user' button in Greasyfork profile pages
 // @include     https://greasyfork.org/*/users/*
 // @include     https://greasyfork.org/*/forum/messages/add
-// @version     2016.10.03
-// @grant       GM_getResourceURL
+// @version     2016.11.4
+// @grant       GM.getResourceUrl
 // @resource    icon http://i.imgur.com/ZU0xS0c.jpg
+// @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // ==/UserScript==
 
 if (window.location.href.indexOf('users') > -1 // if current URL is a profile page
-    && document.querySelector('.user-profile-link > a:nth-child(1)').innerHTML != document.querySelector('div.width-constraint:nth-child(2) > h2:nth-child(1)').innerHTML) { // ... and this profile page is not yours
+	&& document.querySelector('.user-profile-link > a:nth-child(1)').innerHTML != document.querySelector('section.text-content:nth-child(1) > h2:nth-child(1)').innerHTML) { // ... and this profile page is not yours
 
-    var profileName = document.querySelector('div.width-constraint:nth-child(2) > h2:nth-child(1)').innerHTML;
-    sessionStorage.setItem('recipient', profileName); // store in sessionStorage the profileName (it will be inserted in the 'Recipients' textbox ) -after you press the button-
+	var profileName = document.querySelector('section.text-content:nth-child(1) > h2:nth-child(1)');
+	sessionStorage.setItem('recipient', profileName); // store in sessionStorage the profileName (it will be inserted in the 'Recipients' textbox ) -after you press the button-
 
-    var referenceNode = document.querySelector('div.width-constraint:nth-child(2) > h2:nth-child(1)');
-    var a = document.createElement('input');
-    referenceNode.appendChild(a);
+	var referenceNode = document.querySelector('section.text-content:nth-child(1) > h2:nth-child(1)');
+	var a = document.createElement('input');
 
-    a.style.padding = '0px 12px';
-    a.setAttribute('type', 'image');
-    a.setAttribute('src', GM_getResourceURL('icon') );
-    a.id = 'pmButton';
-    a.title = 'Send PM to ' + profileName;
+	a.style.padding = '0px 12px';
+	a.setAttribute('type', 'image');
+	a.id = 'pmButton';
+	a.title = 'Send PM to ' + profileName;
+	(async function() {
+	  a.src = await GM.getResourceUrl('icon');
+	})();
+	referenceNode.appendChild(a);
 
-    document.getElementById('pmButton').addEventListener('click', function() {
-        window.open('https://greasyfork.org/en/forum/messages/add', '_self');
-    });
+
+	document.getElementById('pmButton').addEventListener('click', function() {
+		window.open('https://greasyfork.org/en/forum/messages/add', '_self');
+	});
 
 
 }
@@ -35,5 +39,5 @@ if (window.location.href.indexOf('users') > -1 // if current URL is a profile pa
 
 
 if (window.location.href.indexOf('messages') > -1) { // if current URL is a 'send PM' page
-    document.querySelector('#Form_To').innerHTML = sessionStorage.getItem('recipient'); // .. then insert the stored value of 'recipient' in the 'Recipients' textbox
+	document.querySelector('#Form_To').innerHTML = sessionStorage.getItem('recipient'); // .. then insert the stored value of 'recipient' in the 'Recipients' textbox
 }
