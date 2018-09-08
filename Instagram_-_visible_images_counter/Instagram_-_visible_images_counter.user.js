9332d4b825cbace4947a47202d57ef6474f800dc
darkred <rick3162@gmail.com>
2018-09-08 16:00:53 +0300

    (Instagram - visible images counter) Added a second selector for 'totalString', for when you are not logged in.

--- Instagram_-_visible_images_counter/Instagram_-_visible_images_counter.user.js
+++ Instagram_-_visible_images_counter/Instagram_-_visible_images_counter.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name        Instagram - visible images counter
 // @namespace   darkred
-// @version     2018.8.14
+// @version     2018.9.8
 // @description Shows (in instagram profile pages) how many images out of total (as a number and as a percentage) are currently visible, as you scroll down the page
 // @author      darkred
 // @license     MIT
@@ -39,7 +39,8 @@ var total;
 function showCounter() {
 
 	// var totalString = $(`span:contains('posts'):last-child > span, .g47SY`).html(); 																	// The 'total' value (it's a string). The ".g47SY" selector is for localized pages, e.g. https://www.instagram.com/instagram/?hl=de
-	var totalString = document.querySelector('#react-root > section > main > div > header > section > ul > li:nth-child(1) > span > span').textContent; // The 'total' value (it's a string). The ".g47SY" selector is for localized pages, e.g. https://www.instagram.com/instagram/?hl=de
+	var totalString = document.querySelector(`#react-root > section > main > div > header > section > ul > li:nth-child(1) > span > span,
+											  #react-root > section > main > div > header > section > ul > li:nth-child(1) >   a  > span`).textContent; // The 'total' value (it's a string). The ".g47SY" selector is for localized pages, e.g. https://www.instagram.com/instagram/?hl=de
 	total = totalString.replace(',', '').replace('.', ''); // strip the thousand comma/dot seperator
 
 
