b5d5c23c02504d828430fd09173b437f2920ae66
darkred <rick3162@gmail.com>
2018-09-13 17:18:14 +0300

    (All 'Bullshit Filter' scripts) Reverted === to == and removed the /u flag in the filter regex (per https://greasyfork.org/en/forum/discussion/42612/filters-list-missing-with-firefox-45-9-0-esr-but-is-working-w-ff-52-9-0esr)

--- GreasyFork_Bullshit_Filter/GreasyFork_Bullshit_Filter.user.js
+++ GreasyFork_Bullshit_Filter/GreasyFork_Bullshit_Filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        GreasyFork Bullshit Filter
 // @namespace   darkred
-// @version     2018.9.12.3
+// @version     2018.9.13
 // @description Hides scripts for popular browser games and social networks as well as scripts that use "foreign" characters in descriptions. Applies to posts in Forum too.
 // @author      kuehlschrank, darkred
 // @license     MIT
@@ -24,12 +24,12 @@
 if (window.location.href.indexOf('forum') === -1) {
 	(function() {
 		var filters = {
-			'Non-ASCII': /[^\x00-\x7F\s]+/iu,
-			'Games': /AntiGame|Agar|agar\.io|alis\.io|angel\.io|ExtencionRipXChetoMalo|AposBot|DFxLite|ZTx-Lite|AposFeedingBot|AposLoader|Blah Blah|Orc Clan Script|Astro\s*Empires|^\s*Attack|^\s*Battle|BiteFight|Blood\s*Wars|Bots|Bots4|Brawler|\bBvS\b|Business\s*Tycoon|Castle\s*Age|City\s*Ville|chopcoin\.io|Comunio|Conquer\s*Club|CosmoPulse|cursors\.io|Dark\s*Orbit|Dead\s*Frontier|Diep\.io|\bDOA\b|doblons\.io|DotD|Dossergame|Dragons\s*of\s*Atlantis|driftin\.io|Dugout|\bDS[a-z]+\n|elites\.io|Empire\s*Board|eRep(ublik)?|Epic.*War|ExoPlanet|Falcon Tools|Feuerwache|Farming|FarmVille|Fightinfo|Frontier\s*Ville|Ghost\s*Trapper|Gladiatus|Goalline|Gondal|gota\.io|Grepolis|Hobopolis|\bhwm(\b|_)|Ikariam|\bIT2\b|Jellyneo|Kapi\s*Hospital|Kings\s*Age|Kingdoms?\s*of|knastv(ö|oe)gel|Knight\s*Fight|\b(Power)?KoC(Atta?ck)?\b|\bKOL\b|Kongregate|Last\s*Emperor|Legends?\s*of|Light\s*Rising|lite\.ext\.io|Lockerz|\bLoU\b|Mafia\s*(Wars|Mofo)|Menelgame|Mob\s*Wars|Mouse\s*Hunt|Molehill\s*Empire|NeoQuest|MyFreeFarm|narwhale\.io|Neopets|Nemexia|\bOGame\b|Ogar(io)?|Pardus|Pennergame|Pigskin\s*Empire|PlayerScripts|pokeradar\.io|Popmundo|Po?we?r\s*(Bot|Tools)|PsicoTSI|Ravenwood|Schulterglatze|slither\.io|slitherplus\.io|slitheriogameplay|SpaceWars|splix\.io|\bSW_[a-z]+\n|\bSnP\b|The\s*Crims|The\s*West|torto\.io|Travian|Treasure\s*Isl(and|e)|Tribal\s*Wars|TW.?PRO|Vampire\s*Wars|vertix\.io|War\s*of\s*Ninja|West\s*Wars|wings\.io|\bWoD\b|World\s*of\s*Dungeons|wtf\s*battles|Wurzelimperium/iu,
-			'Social Networks': /Face\s*book|Google(\+| Plus)|\bHabbo|Kaskus|\bLepra|Leprosorium|MySpace|meinVZ|odnoklassniki|Одноклассники|Orkut|sch(ue|ü)ler(VZ|\.cc)?|studiVZ|Unfriend|Valenth|VK|vkontakte|ВКонтакте|Qzone|Twitter|TweetDeck/iu,
-			'Clutter': /^\s*(.{1,3})\1+\n|^\s*(.+?)\n+\2\n*$|^\s*.{1,5}\n|do\s*n('|o)?t (install|download)|nicht installieren|just(\s*a)?\s*test|^\s*.{0,4}test.{0,4}\n|\ntest(ing)?\s*|^\s*(\{@|Smolka|Hacks)|\[\d{4,5}\]|free\s*download|theme|(night|dark) ?(mode)?/iu
+			'Non-ASCII': /[^\x00-\x7F\s]+/i,
+			'Games': /AntiGame|Agar|agar\.io|alis\.io|angel\.io|ExtencionRipXChetoMalo|AposBot|DFxLite|ZTx-Lite|AposFeedingBot|AposLoader|Blah Blah|Orc Clan Script|Astro\s*Empires|^\s*Attack|^\s*Battle|BiteFight|Blood\s*Wars|Bots|Bots4|Brawler|\bBvS\b|Business\s*Tycoon|Castle\s*Age|City\s*Ville|chopcoin\.io|Comunio|Conquer\s*Club|CosmoPulse|cursors\.io|Dark\s*Orbit|Dead\s*Frontier|Diep\.io|\bDOA\b|doblons\.io|DotD|Dossergame|Dragons\s*of\s*Atlantis|driftin\.io|Dugout|\bDS[a-z]+\n|elites\.io|Empire\s*Board|eRep(ublik)?|Epic.*War|ExoPlanet|Falcon Tools|Feuerwache|Farming|FarmVille|Fightinfo|Frontier\s*Ville|Ghost\s*Trapper|Gladiatus|Goalline|Gondal|gota\.io|Grepolis|Hobopolis|\bhwm(\b|_)|Ikariam|\bIT2\b|Jellyneo|Kapi\s*Hospital|Kings\s*Age|Kingdoms?\s*of|knastv(ö|oe)gel|Knight\s*Fight|\b(Power)?KoC(Atta?ck)?\b|\bKOL\b|Kongregate|Last\s*Emperor|Legends?\s*of|Light\s*Rising|lite\.ext\.io|Lockerz|\bLoU\b|Mafia\s*(Wars|Mofo)|Menelgame|Mob\s*Wars|Mouse\s*Hunt|Molehill\s*Empire|NeoQuest|MyFreeFarm|narwhale\.io|Neopets|Nemexia|\bOGame\b|Ogar(io)?|Pardus|Pennergame|Pigskin\s*Empire|PlayerScripts|pokeradar\.io|Popmundo|Po?we?r\s*(Bot|Tools)|PsicoTSI|Ravenwood|Schulterglatze|slither\.io|slitherplus\.io|slitheriogameplay|SpaceWars|splix\.io|\bSW_[a-z]+\n|\bSnP\b|The\s*Crims|The\s*West|torto\.io|Travian|Treasure\s*Isl(and|e)|Tribal\s*Wars|TW.?PRO|Vampire\s*Wars|vertix\.io|War\s*of\s*Ninja|West\s*Wars|wings\.io|\bWoD\b|World\s*of\s*Dungeons|wtf\s*battles|Wurzelimperium/i,
+			'Social Networks': /Face\s*book|Google(\+| Plus)|\bHabbo|Kaskus|\bLepra|Leprosorium|MySpace|meinVZ|odnoklassniki|Одноклассники|Orkut|sch(ue|ü)ler(VZ|\.cc)?|studiVZ|Unfriend|Valenth|VK|vkontakte|ВКонтакте|Qzone|Twitter|TweetDeck/i,
+			'Clutter': /^\s*(.{1,3})\1+\n|^\s*(.+?)\n+\2\n*$|^\s*.{1,5}\n|do\s*n('|o)?t (install|download)|nicht installieren|just(\s*a)?\s*test|^\s*.{0,4}test.{0,4}\n|\ntest(ing)?\s*|^\s*(\{@|Smolka|Hacks)|\[\d{4,5}\]|free\s*download|theme|(night|dark) ?(mode)?/i
 		};
-		if (typeof GM_getValue === 'undefined' || (typeof GM_getValue.toString === 'function' && GM_getValue.toString().indexOf('not supported') > -1)) {
+		if (typeof GM_getValue == 'undefined' || (typeof GM_getValue.toString == 'function' && GM_getValue.toString().indexOf('not supported') > -1)) {
 			GM_getValue = my_GM_getValue;
 			GM_setValue = my_GM_setValue;
 		}
@@ -59,7 +59,7 @@ if (window.location.href.indexOf('forum') === -1) {
 		function filterScripts() {
 			var activeFilters = [];
 			for (var filter in filters) {
-				if (filters.hasOwnProperty(filter) && GM_getValue(filter, 'on') === 'on') {
+				if (filters.hasOwnProperty(filter) && GM_getValue(filter, 'on') == 'on') {
 					activeFilters.push(filters[filter]);
 				}
 			}
@@ -84,7 +84,7 @@ if (window.location.href.indexOf('forum') === -1) {
 			span.className = 'filter-switches';
 			for (var filter in filters) {
 				if (filters.hasOwnProperty(filter)) {
-					span.appendChild(createSwitch(filter, GM_getValue(filter, 'on') === 'on'));
+					span.appendChild(createSwitch(filter, GM_getValue(filter, 'on') == 'on'));
 				}
 			}
 			document.querySelector('.filter-status').parentNode.appendChild(span);
@@ -95,7 +95,7 @@ if (window.location.href.indexOf('forum') === -1) {
 			a.className = isOn ? 'filter-on' : 'filter-off';
 			a.textContent = label;
 			a.addEventListener('click', function(e) {
-				if (this.className === 'filter-on') {
+				if (this.className == 'filter-on') {
 					this.className = 'filter-off';
 					GM_setValue(this.textContent, 'off');
 				} else {
@@ -130,7 +130,7 @@ if (window.location.href.indexOf('forum') === -1) {
 			'Social Networks': /Face\s*book|Google(\+| Plus)|\bHabbo|Kaskus|\bLepra|Leprosorium|MySpace|meinVZ|odnoklassniki|Одноклассники|Orkut|sch(ue|ü)ler(VZ|\.cc)?|studiVZ|Unfriend|Valenth|VK|vkontakte|ВКонтакте|Qzone|Twitter|TweetDeck/iu,
 			'Clutter': /^\s*(.{1,3})\1+\n|^\s*(.+?)\n+\2\n*$|^\s*.{1,5}\n|do\s*n('|o)?t (install|download)|nicht installieren|just(\s*a)?\s*test|^\s*.{0,4}test.{0,4}\n|\ntest(ing)?\s*|^\s*(\{@|Smolka|Hacks)|\[\d{4,5}\]|free\s*download/iu
 		};
-		if (typeof GM_getValue === 'undefined' || (typeof GM_getValue.toString === 'function' && GM_getValue.toString().indexOf('not supported') > -1)) {
+		if (typeof GM_getValue == 'undefined' || (typeof GM_getValue.toString == 'function' && GM_getValue.toString().indexOf('not supported') > -1)) {
 			GM_getValue = my_GM_getValue;
 			GM_setValue = my_GM_setValue;
 		}
@@ -160,7 +160,7 @@ if (window.location.href.indexOf('forum') === -1) {
 		function filterScripts() {
 			var activeFilters = [];
 			for (var filter in filters) {
-				if (filters.hasOwnProperty(filter) && GM_getValue(filter, 'on') === 'on') {
+				if (filters.hasOwnProperty(filter) && GM_getValue(filter, 'on') == 'on') {
 					activeFilters.push(filters[filter]);
 				}
 			}
@@ -186,7 +186,7 @@ if (window.location.href.indexOf('forum') === -1) {
 			span.className = 'filter-switches';
 			for (var filter in filters) {
 				if (filters.hasOwnProperty(filter)) {
-					span.appendChild(createSwitch(filter, GM_getValue(filter, 'on') === 'on'));
+					span.appendChild(createSwitch(filter, GM_getValue(filter, 'on') == 'on'));
 				}
 			}
 			document.querySelector('.filter-status').parentNode.appendChild(span);
@@ -197,7 +197,7 @@ if (window.location.href.indexOf('forum') === -1) {
 			a.className = isOn ? 'filter-on' : 'filter-off';
 			a.textContent = label;
 			a.addEventListener('click', function(e) {
-				if (this.className === 'filter-on') {
+				if (this.className == 'filter-on') {
 					this.className = 'filter-off';
 					GM_setValue(this.textContent, 'off');
 				} else {
