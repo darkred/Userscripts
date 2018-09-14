b53f2542edb8dc8c2c0356d754656fd3cb84defb
darkred <rick3162@gmail.com>
2018-09-14 19:59:00 +0300

    (all RARBG scripts) Added rarbgproxied.org in the '@include' rule. per https://openuserjs.org/scripts/darkred/RARBG_-_torrent_and_magnet_links/issues/Does_not_work_1.9.2018#comment-165b42ba120

--- RARBG_-_torrent_and_magnet_links/RARBG_-_torrent_and_magnet_links.user.js
+++ RARBG_-_torrent_and_magnet_links/RARBG_-_torrent_and_magnet_links.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        RARBG - torrent and magnet links
 // @namespace   darkred
-// @version     2018.9.11
+// @version     2018.9.14
 // @description Adds a column with torrent and magnet links in RARBG lists
 // @author      darkred
 // @contributor sxe
 // @license     MIT
-// @include     /^(https?:)?\/\/(www\.)?(rarbg(\.(bypassed|unblockall|unblocked))?|rarbgaccess|rarbgget|rarbgmirror|rarbgproxy|rarbgprx|rarbgs|rarbgto|rarbgunblock|proxyrarbg|unblocktorrent)\.(to|com|org|is|xyz|lol|vc|link)\/(rarbg-proxy-unblock\/)?(torrents\.php.*|catalog\/.*|top10)$/
+// @include     /^(https?:)?\/\/(www\.)?(rarbg(\.(bypassed|unblockall|unblocked))?|rarbgaccess|rarbgget|rarbgmirror|rarbgproxy|rarbgproxied|rarbgprx|rarbgs|rarbgto|rarbgunblock|proxyrarbg|unblocktorrent)\.(to|com|org|is|xyz|lol|vc|link)\/(rarbg-proxy-unblock\/)?(torrents\.php.*|catalog\/.*|top10)$/
 // @grant       none
 // ==/UserScript==
 
