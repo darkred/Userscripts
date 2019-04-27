8ae85a5c9312cfa74d27aa2404eebb32ec712283
darkred <rick3162@gmail.com>
2019-04-27 22:42:58 +0300

    (RARBG - torrent and magnet links) Now it works in TV browser too (per request https://greasyfork.org/en/forum/discussion/52949/x )

--- RARBG_-_torrent_and_magnet_links/RARBG_-_torrent_and_magnet_links.user.js
+++ RARBG_-_torrent_and_magnet_links/RARBG_-_torrent_and_magnet_links.user.js
@@ -1,95 +1,130 @@
 // ==UserScript==
 // @name        RARBG - torrent and magnet links
 // @namespace   darkred
-// @version     2018.9.14
+// @version     2019.4.27
 // @description Adds a column with torrent and magnet links in RARBG lists
 // @author      darkred
 // @contributor sxe
 // @license     MIT
-// @include     /^(https?:)?\/\/(www\.)?(rarbg(\.(bypassed|unblockall|unblocked))?|rarbgaccess|rarbgget|rarbgmirror|rarbgproxy|rarbgproxied|rarbgprx|rarbgs|rarbgto|rarbgunblock|proxyrarbg|unblocktorrent)\.(to|com|org|is|xyz|lol|vc|link)\/(rarbg-proxy-unblock\/)?(torrents\.php.*|catalog\/.*|top10)$/
+// @include     /^(https?:)?\/\/(www\.)?(rarbg(\.(bypassed|unblockall|unblocked))?|rarbgaccess|rarbgget|rarbgmirror|rarbgproxy|rarbgproxied|rarbgprx|rarbgs|rarbgto|rarbgunblock|proxyrarbg|unblocktorrent)\.(to|com|org|is|xyz|lol|vc|link)\/(rarbg-proxy-unblock\/)?(torrents\.php.*|catalog\/.*|tv\/.*|top10)$/
 // @grant       none
+// @require     https://raw.githubusercontent.com/uzairfarooq/arrive/master/src/arrive.js
+// @run-at      document-idle
 // ==/UserScript==
 
 
-function appendColumn(title) {
+function appendColumn(elem) {
 
-	var entries = document.querySelectorAll('.lista2t > tbody > tr > td:nth-child(2) ');        // the initial column 'Files' after of which the extra column will be appended
+	var title = 'DL&nbsp;ML';
+
+	var entries = elem.querySelectorAll('.lista2t > tbody > tr > td:nth-child(2) ');        // the initial column 'Files' after of which the extra column will be appended
 
 	for (let i = 0; i < entries.length; i++) {                                                  // creation of the extra column
 		entries[i].insertAdjacentHTML('afterend', `<td>` + title + `</td>`);
 	}
 
-	var header = document.querySelector('.lista2t > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)');       // the first cell (the header cell) of the new column
+	var header = elem.querySelector('.lista2t > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)');       // the first cell (the header cell) of the new column
 	header.innerHTML = title;
-	header.setAttribute('class', 'header6');
 	header.setAttribute('align', 'center');
+	header.setAttribute('class', 'header6');
 
-	var cells = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');               // the rest cells of the new column
+	var cells = elem.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');               // the rest cells of the new column
 	for (let i = 0; i < cells.length; i++) {
 		cells[i].setAttribute('class', 'lista');
 		cells[i].setAttribute('width', '50px');
 		cells[i].setAttribute('align', 'center');
 	}
 
-	var newColumn = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');       // new column
-	var oldColumn = document.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(2)');       // old column
+	var newColumn = elem.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');       // new column
+	var oldColumn = elem.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(2)');       // old column
 
-	// populate the cells in the new column with DL and ML links
+
 	for (let i = 0; i < newColumn.length; i++) {
-		if ((/over\/(.*)\.jpg\\/).test(oldColumn[i].firstChild.outerHTML)){
-			var hash = oldColumn[i].firstChild.outerHTML.match(/over\/(.*)\.jpg\\/)[1];
-		} else {
-			hash = undefined;
-		}
-		let title = oldColumn[i].firstChild.innerText;
-		var trackers = 'http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710&tr=udp%3A%2F%2F9.rarbg.to%3A2710';
-		newColumn[i].innerHTML = '<a href="' + oldColumn[i].firstChild.href.replace('torrent/', 'download.php?id=') + '&f=' + oldColumn[i].firstChild.innerText + '-[rarbg.com].torrent"><img src="https://dyncdn.me/static/20/img/16x16/download.png"">' + '</>';
-		// if the torrent hash is contained in the filenames of the thumbnail image
-		if (hash !== undefined){
-			// then generate magnet link from preview thumbnail if available
-			newColumn[i].innerHTML += '&nbsp;<a href="magnet:?xt=urn:btih:' + hash + '&dn=' + title + '&tr=' + trackers + ' "><img src="https://dyncdn.me/static/20/img/magnet.gif""></>';
-		} else {
-			// else generate it via an ajax request
-			let href = oldColumn[i].firstChild.href;
-			newColumn[i].innerHTML += '&nbsp;<a class="xhrMagnetLink" data-href="' + href + '" href="#"><img src="https://dyncdn.me/static/20/img/magnet.gif""></>';
-			newColumn[i].lastChild.title = 'ML via XHR';
-		}
+
+		let href = oldColumn[i].firstChild.href;
+
+		newColumn[i].innerHTML =        '<a class="xhrDownloadLink" data-href="' + href + '" href="#"><img src="https://dyncdn.me/static/20/img/16x16/download.png""></>';
+		newColumn[i].lastChild.title = 'DL via XHR';
+
+		newColumn[i].innerHTML += '&nbsp;<a class="xhrMagnetLink" data-href="' + href + '" href="#"><img src="https://dyncdn.me/static/20/img/magnet.gif""></>';
+		newColumn[i].lastChild.title = 'ML via XHR';
+
 	}
 }
 
-appendColumn('DL&nbsp;ML');
 
-var xhrMagnetLinks = document.querySelectorAll('.xhrMagnetLink');
 
-for(let i=0; i < xhrMagnetLinks.length; i++) {
+function addMouseoverListeners(links, type){
 
-	xhrMagnetLinks[i].addEventListener('mouseover', function(event){
+	for(let i = 0; i < links.length; i++) {
 
-		event.preventDefault();
-		let href = this.getAttribute('href');
-		if (href === '#') {
-			let tLink = this.getAttribute('data-href');
+		links[i].addEventListener('mouseover', function(event){
 
-			var xhr = new XMLHttpRequest();
-			xhr.open('GET', tLink, true);	// XMLHttpRequest.open(method, url, async)
-			xhr.onload = function () {
+			event.preventDefault();
+			let href = this.getAttribute('href');
+			if (href === '#') {
+				let tLink = this.getAttribute('data-href');
 
-				let container = document.implementation.createHTMLDocument().documentElement;
-				container.innerHTML = xhr.responseText;
-				let magnetLink = container.querySelector('a[href^="magnet:"]');		// the 'magnet link' element in the retrieved page
+				var xhr = new XMLHttpRequest();
+				xhr.open('GET', tLink, true);	// XMLHttpRequest.open(method, url, async)
+				xhr.onload = function () {
 
-				if (magnetLink) {
-					let magnetHref = magnetLink.href;
-					this.href = magnetHref;
-					let currentMagnetLink = document.querySelector('a[data-href^="' + tLink + '"]');	// the current magnet link element
-					currentMagnetLink.setAttribute('href', magnetHref);
-				}
+					let container = document.implementation.createHTMLDocument().documentElement;
+					container.innerHTML = xhr.responseText;
 
-			};
-			xhr.send();
+					let retrievedLink;
+					if (type === 'dl'){
+						retrievedLink = container.querySelector('a[href^="/download.php"]');		// the 'magnet link' element in the retrieved page
+					} else {
+						retrievedLink = container.querySelector('a[href^="magnet:"]');		// the 'magnet link' element in the retrieved page
+					}
 
-		}
 
-	}, false);
+					if (retrievedLink) {
+						links[i].setAttribute('href', retrievedLink.href);
+					}
 
+
+
+				};
+				xhr.send();
+
+			}
+
+		}, false);
+
+	}
+
+}
+
+
+
+function createColumn(element){
+	appendColumn(element);
+	addMouseoverListeners(element.querySelectorAll('.xhrDownloadLink'), 'dl' );
+	addMouseoverListeners(element.querySelectorAll('.xhrMagnetLink'), 'ml' );
+}
+
+
+
+
+
+const isOnTV = window.location.href.includes('/tv/');  // example link: https://rarbgproxy.org/tv/tt1720601/
+
+if (isOnTV) {
+	var tvLinks = document.querySelectorAll('.tvshowClick');
+
+	for (let i = 0; i < tvLinks.length; i++) {
+		tvLinks[i].addEventListener('click',
+			function handler(e) {  // https://stackoverflow.com/questions/26617719/turn-off-event-listener-after-triggered-once
+				var targetElement = e.target || e.srcElement;   // https://stackoverflow.com/questions/11741070/js-how-to-get-the-element-clicked-on
+				targetElement.parentElement.nextElementSibling.arrive('.lista2t', function() {
+					createColumn(targetElement.parentElement.nextElementSibling);
+					targetElement.parentElement.nextElementSibling.unbindArrive('.lista2t');
+				});
+			}
+		);
+	}
+} else {
+	createColumn(document);        // the initial column 'Files' after of which the extra column will be appended);
 }
