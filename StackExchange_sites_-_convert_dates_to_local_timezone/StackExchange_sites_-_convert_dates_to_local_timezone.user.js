40ca39a4707a38a889558d31f9762081e59fc5a0
darkred <rick3162@gmail.com>
2019-08-26 13:19:01 +0300

    (StackExchange sites - convert dates to local timezone) - now it works in reputation tab entries too (per GF discussion) - replaced the various @match rules with one @include rule with a regex,  in order to include all 174 SO sites

--- StackExchange_sites_-_convert_dates_to_local_timezone/StackExchange_sites_-_convert_dates_to_local_timezone.user.js
+++ StackExchange_sites_-_convert_dates_to_local_timezone/StackExchange_sites_-_convert_dates_to_local_timezone.user.js
@@ -1,17 +1,11 @@
 // ==UserScript==
 // @name        StackExchange sites - convert dates to local timezone
 // @namespace   darkred
-// @version     2016.10.16
+// @version     2019.8.26
 // @description Converts dates to your local timezone
 // @author      darkred
 // @license     MIT
-// @match       *://*.stackoverflow.com/*
-// @match       *://*.stackexchange.com/*
-// @match       *://*.superuser.com/*
-// @match       *://*.stackapps.com/*
-// @match       *://*.askubuntu.com/*
-// @match       *://*.mathoverflow.net/*
-// @match       *://*.serverfault.com/*
+// @include     /^(https?:)?\/\/(www\.)?((3dprinting|academia|ai|alcohol|android|anime|apple|arduino|astronomy|aviation|bicycles|bioinformatics|biology|bitcoin|blender|boardgames|bricks|buddhism|chemistry|chess|chinese|christianity|civicrm|codegolf|codereview|coffee|communitybuilding|computergraphics|conlang|cooking|craftcms|crafts|crypto|cs|cseducators|cstheory|datascience|dba|devops|diy|drupal|dsp|earthscience|ebooks|economics|electronics|elementaryos|ell|emacs|engineering|english|eosio|esperanto|ethereum|expatriates|expressionengine|fitness|freelancing|french|gamedev|gaming|gardening|genealogy|german|gis|graphicdesign|ham|hardwarerecs|hermeneutics|hinduism|history|homebrew|hsm|interpersonal|iot|iota|islam|italian|japanese|joomla|judaism|korean|languagelearning|latin|law|lifehacks|linguistics|literature|magento|martialarts|math|matheducators|mathematica|mechanics|medicalsciences|meta|monero|money|movies|music|musicfans|mythology|networkengineering|opendata|opensource|or|outdoors|parenting|patents|pets|philosophy|photo|physics|pm|poker|politics|portuguese|psychology|puzzling|quant|quantumcomputing|raspberrypi|retrocomputing|reverseengineering|robotics|rpg|rus|russian|salesforce|scicomp|scifi|security|sharepoint|sitecore|skeptics|softwareengineering|softwarerecs|sound|space|spanish|sports|sqa|stats|stellar|sustainability|tex|tezos|tor|travel|tridion|ukrainian|unix|ux|vegetarianism|vi|video|webapps|webmasters|windowsphone|woodworking|wordpress|workplace|worldbuilding|writing)\.stackexchange\.com|(es|ja|pt|ru)\.stackoverflow\.com|(askubuntu|serverfault|stackapps|stackoverflow|superuser)\.com|mathoverflow\.net).*$/
 // @grant       none
 // @require     http://momentjs.com/downloads/moment.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.6/moment-timezone-with-data-2010-2020.js
@@ -27,7 +21,7 @@ var serverTimezone = 'Europe/Berlin';		// GMT+1
 
 
 function convertDates() {
-	var dates = document.getElementsByClassName('relativetime');
+	var dates = document.querySelectorAll('.relativetime, .rep-time');
 	var temp;
 	for (var i = 0; i < dates.length; i++) {
 		temp = moment(dates[i].title, 'YYYY-MM-DD HH:mm:ssZ', true);
@@ -40,7 +34,7 @@ function convertDates() {
 
 convertDates();
 
-var target = document.querySelector('#question-mini-list'),
+var target = document.querySelectorAll('#question-mini-list, .rep-breakdown')[0],
 	observer = new MutationObserver(function (mutations) {
 		convertDates();
 	}),
