fa792083c83cf595dc343f162e5e1f9d3b875c0c
darkred <rick3162@gmail.com>
2019-09-17 00:31:50 +0300

    (RARBG - torrent and magnet links) - replaced '#' hrefs with 'javascript:void(0);' - changed addEventListener to 'click' instead of 'mouseover'
    Both per https://greasyfork.org/en/forum/discussion/63152/ .
    Thanks goes to dandyclubs.

--- RARBG_-_torrent_and_magnet_links/RARBG_-_torrent_and_magnet_links.user.js
+++ RARBG_-_torrent_and_magnet_links/RARBG_-_torrent_and_magnet_links.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        RARBG - torrent and magnet links
 // @namespace   darkred
-// @version     2019.8.13
+// @version     2019.9.17
 // @description Adds a column with torrent and magnet links in RARBG lists
 // @author      darkred
-// @contributor sxe
+// @contributor sxe, dandyclubs
 // @license     MIT
 // @include     /^(https?:)?\/\/(www\.)?(rarbg|rbg(\.(bypassed|unblockall|unblocked))?|rarbgaccess(ed)?|rarbgget|rarbgmirror(ed)?|rarbgproxy|rarbgproxied|rarbgprx|rarbgs|rarbgto|rarbgunblock|rarbgenter|proxyrarbg|unblocktorrent)\.(to|com|org|is|xyz|lol|vc|link)\/(rarbg-proxy-unblock\/)?(torrents\.php.*|catalog\/.*|tv\/.*|top10)$/
 // @grant       none
@@ -15,38 +15,38 @@
 
 function appendColumn(elem) {
 
-	var title = 'DL&nbsp;ML';
+	const title = 'DL&nbsp;ML';
 
-	var entries = elem.querySelectorAll('.lista2t > tbody > tr > td:nth-child(2) ');        // the initial column 'Files' after of which the extra column will be appended
+	let entries = elem.querySelectorAll('.lista2t > tbody > tr > td:nth-child(2) ');        // the initial column 'Files' after of which the extra column will be appended
 
 	for (let i = 0; i < entries.length; i++) {                                                  // creation of the extra column
 		entries[i].insertAdjacentHTML('afterend', `<td>` + title + `</td>`);
 	}
 
-	var header = elem.querySelector('.lista2t > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)');       // the first cell (the header cell) of the new column
+	let header = elem.querySelector('.lista2t > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3)');       // the first cell (the header cell) of the new column
 	header.innerHTML = title;
 	header.setAttribute('align', 'center');
 	header.setAttribute('class', 'header6');
 
-	var cells = elem.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');               // the rest cells of the new column
+	let cells = elem.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');               // the rest cells of the new column
 	for (let i = 0; i < cells.length; i++) {
 		cells[i].setAttribute('class', 'lista');
 		cells[i].setAttribute('width', '50px');
 		cells[i].setAttribute('align', 'center');
 	}
 
-	var newColumn = elem.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');       // new column
-	var oldColumn = elem.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(2)');       // old column
+	let newColumn = elem.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(3)');       // new column
+	let oldColumn = elem.querySelectorAll('.lista2t > tbody > tr[class="lista2"] > td:nth-child(2)');       // old column
 
 
 	for (let i = 0; i < newColumn.length; i++) {
 
 		let href = oldColumn[i].firstChild.href;
 
-		newColumn[i].innerHTML =        '<a class="xhrDownloadLink" data-href="' + href + '" href="#"><img src="https://dyncdn.me/static/20/img/16x16/download.png""></>';
+		newColumn[i].innerHTML =        '<a class="xhrDownloadLink" data-href="' + href + '" href="javascript:void(0);"><img src="https://dyncdn.me/static/20/img/16x16/download.png""></>';
 		newColumn[i].lastChild.title = 'DL via XHR';
 
-		newColumn[i].innerHTML += '&nbsp;<a class="xhrMagnetLink" data-href="' + href + '" href="#"><img src="https://dyncdn.me/static/20/img/magnet.gif""></>';
+		newColumn[i].innerHTML += '&nbsp;<a class="xhrMagnetLink" data-href="' + href + '" href="javascript:void(0);"><img src="https://dyncdn.me/static/20/img/magnet.gif""></>';
 		newColumn[i].lastChild.title = 'ML via XHR';
 
 	}
@@ -54,15 +54,14 @@ function appendColumn(elem) {
 
 
 
-function addMouseoverListeners(links, type){
+function addClickListeners(links, type){
 
 	for(let i = 0; i < links.length; i++) {
 
-		links[i].addEventListener('mouseover', function(event){
+		links[i].addEventListener('click', function(event){
 
-			event.preventDefault();
 			let href = this.getAttribute('href');
-			if (href === '#') {
+			if (href === 'javascript:void(0);') {
 				let tLink = this.getAttribute('data-href');
 
 				var xhr = new XMLHttpRequest();
@@ -82,8 +81,8 @@ function addMouseoverListeners(links, type){
 
 					if (retrievedLink) {
 						links[i].setAttribute('href', retrievedLink.href);
+						links[i].click();
 					}
-
 
 
 				};
@@ -101,8 +100,8 @@ function addMouseoverListeners(links, type){
 
 function createColumn(element){
 	appendColumn(element);
-	addMouseoverListeners(element.querySelectorAll('.xhrDownloadLink'), 'dl' );
-	addMouseoverListeners(element.querySelectorAll('.xhrMagnetLink'), 'ml' );
+	addClickListeners(element.querySelectorAll('.xhrDownloadLink'), 'dl' );
+	addClickListeners(element.querySelectorAll('.xhrMagnetLink'), 'ml' );
 }
 
 
