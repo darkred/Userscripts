6702644dbb39fa6e8d2bb878f25fa332b6907d2b
darkred <rick3162@gmail.com>
2019-10-11 19:50:24 +0300

    (Google YouTube search link) Added SVG YouTube icon (per request)

--- Google_youtube_search_link/Google_youtube_search_link.user.js
+++ Google_youtube_search_link/Google_youtube_search_link.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Google YouTube search link
 // @namespace    darkred
-// @version      2017.6.9
-// @description  Adds a YouTube search link next to the Videos link (e.g. Web, Images, Videos, YouTube, News, Maps, Shopping, ...)
-// @authors      wOxxOm, darkred
+// @author       wOxxOm, darkred
 // @license      MIT
+// @description  Adds a YouTube search link next to the Videos link (e.g. Web, Images, Videos, YouTube, News, Maps, Shopping, ...)
+// @version      2019.10.11
 // @include      https://www.google.com/*
 // @include      /https?:\/\/(www\.)?google\.(com|(?:com?\.)?\w\w)\/.*/
 // @grant        none
@@ -42,7 +42,7 @@ function process(mutations) {
 					}
 				}).observe(queryElement, { attributes: true, attributeFilter: ['value'] });			// monitor the textbox for changes (your typed criteria)
 			}
-		} else if ((q = location.href.match(/^.+?(?:[#\/&?](?:q|query))=(.+?)(?:|&.+|\|.+)$/)))
+		} else if ((q = location.href.match(/^.+?(?:[#/&?](?:q|query))=(.+?)(?:|&.+|\|.+)$/)))
 			q = q[1];
 
 		var node = document.querySelector(`a[href*='tbm=vid']`);			// selector for the 'Videos' link (works in any Google search page language)
@@ -50,6 +50,32 @@ function process(mutations) {
 					'<a class="q qs" href="https://www.youtube.com/results?search_query=' + q + '">YouTube</a>' +
 					'</div>';
 		node.parentElement.insertAdjacentHTML('afterend', text);		// insert the YouTube link
+
+
+		// Source URL: https://upload.wikimedia.org/wikipedia/commons/c/c9/YouTube_play_buttom_dark_icon_%282013-2017%29.svg
+		// Line 60 initially:   y="0px" viewBox="0 0 1024 721" enable-background="new 0 0 1024 721" xml:space="preserve">
+		var svg =
+		`
+		<svg version="1.1" id="YouTube_Icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+			 y="0px" viewBox="0 -150 1024 721" width="16" height="16" preserveAspectRatio="xMidYMid meet" enable-background="new 0 -150 1024 721" xml:space="preserve">
+		<path id="Triangle" fill="#FFFFFF" d="M407,493l276-143L407,206V493z"/>
+		<path id="The_Sharpness" opacity="0.12" fill-rule="evenodd" clip-rule="evenodd" d="M407,206l242,161.6l34-17.6L407,206z"/>
+		<g id="Lozenge">
+			<g>
+				<path fill="#282928" d="M1013,156.3c0,0-10-70.4-40.6-101.4C933.6,14.2,890,14,870.1,11.6C727.1,1.3,512.7,1.3,512.7,1.3h-0.4
+					c0,0-214.4,0-357.4,10.3C135,14,91.4,14.2,52.6,54.9C22,85.9,12,156.3,12,156.3S1.8,238.9,1.8,321.6v77.5
+					C1.8,481.8,12,564.4,12,564.4s10,70.4,40.6,101.4c38.9,40.7,89.9,39.4,112.6,43.7c81.7,7.8,347.3,10.3,347.3,10.3
+					s214.6-0.3,357.6-10.7c20-2.4,63.5-2.6,102.3-43.3c30.6-31,40.6-101.4,40.6-101.4s10.2-82.7,10.2-165.3v-77.5
+					C1023.2,238.9,1013,156.3,1013,156.3z M407,493l0-287l276,144L407,493z"/>
+			</g>
+		</g>
+		</svg>
+		`;
+
+		document.querySelector('#__YOUTUBE_SEARCH__ > a').insertAdjacentHTML('beforebegin', svg);
+
+
+
 	}
 
 	new MutationObserver(process).observe(menuContainer, { childList: true });
