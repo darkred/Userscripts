edbd662b42feede66bc0020b3a05a4206913b845
darkred <rick3162@gmail.com>
2019-11-13 19:42:16 +0200

    Libraries update

--- Rotten_Tomatoes_Decimal_Rating/setMutationHandler.js
+++ Rotten_Tomatoes_Decimal_Rating/setMutationHandler.js
@@ -1,54 +1,96 @@
-/* EXAMPLE:
-	
-	setMutationHandler(document, '.container p.some-child', function(nodes) {
-		// single node:
-		nodes[0].remove();
-		
-		// or multiple nodes:
-		nodes.forEach(function(node) {
-			node.style.display = 'none';
-		});
-
-		//this.disconnect(); // disconnect the observer, this is useful for one-time jobs
-		return true; // continue enumerating current batch of mutations
-	});
-*/
-
 // ==UserScript==
 // @name          setMutationHandler
 // @description   MutationObserver wrapper to wait for the specified CSS selector
 // @namespace     wOxxOm.scripts
 // @author        wOxxOm
 // @grant         none
-// @version       2.0.4
+// @version       3.0.2
 // ==/UserScript==
 
-function setMutationHandler(baseNode, selector, cb, options) {
-	var ob = new MutationObserver(function(mutations) {
-		for (var i=0, ml=mutations.length, m; (i<ml) && (m=mutations[i]); i++)
+function setMutationHandler(target, selector, handler, options) {
+// or    setMutationHandler(selector, handler, options) {
+// or    setMutationHandler(options) {
+	if (typeof arguments[0] == 'string') {
+		options = arguments[2] || {};
+		handler = arguments[1];
+		selector = arguments[0];
+		target = document;
+	} else if (arguments.length == 1 && target && typeof target.handler == 'function') {
+		options = arguments[0];
+		handler = options.handler;
+		selector = options.selector;
+		target = options.target || document;
+	} else if (!(target instanceof Node)) {
+		throw 'Bad params for setMutationHandler.\n' +
+			'A: [optional Node] target, [String] selector, [Function] handler, [optional Object] options\n' +
+			'B: [Object] options\n' +
+			'Options: target, selector, handler, processExisting, childList, attributes, characterData, subtree, attributeOldValue, characterDataOldValue, attributeFilter';
+	} else
+    	options = options || {};
+
+	if (options.processExisting && target.querySelector(selector))
+		handler.call(null, Array.prototype.slice.call(target.querySelectorAll(selector)));
+	if (!options.attributes && !options.characterData && !options.childList && options.subtree === undefined)
+		options.childList = options.subtree = true;
+
+	var cb;
+	if (/^#[\w\d-]+$/.test(selector)) {
+		selector = selector.substr(1);
+		cb = MOhandlerForId;
+	} else {
+		cb = MOhandler;
+	}
+	var observer = new MutationObserver(cb);
+	observer.observe(target, options || {subtree:true, childList:true});
+	return observer;
+
+	function MOhandler(mutations) {
+		if (mutations.length > 100 && !document.querySelector(selector))
+			return;
+		var found = [];
+		for (var i=0, m; (m = mutations[i++]); ) {
 			switch (m.type) {
 				case 'childList':
-					if (m.addedNodes[0] && m.addedNodes[0].nodeType == 3) { // Node.TEXT_NODE
-						if (m.target.matches(selector) && !cb.call(ob, [m.target], m))
-							return;
-						continue;
+					var nodes = m.addedNodes, nl = nodes.length;
+					var textNodesOnly = true;
+					for (var j=0; j < nl; j++) {
+						var n = nodes[j];
+						textNodesOnly &= n.nodeType == 3; // TEXT_NODE
+						if (n.nodeType != 1) // ELEMENT_NODE
+							continue;
+						if (n.matches(selector))
+							found.push(n);
+						else if (n.querySelector(selector)) {
+							n = n.querySelectorAll(selector);
+							if (n.length < 1000)
+								found.push.apply(found, n);
+							else
+								found = found.concat(found.slice.call(n));
+						}
 					}
-					for (var j=0, nodes=m.addedNodes, nl=nodes.length, n; (j<nl) && (n=nodes[j]); j++)
-						if (n.nodeType == 1) 
-							if ((n = n.matches(selector) ? [n] : n.querySelectorAll(selector)) && n.length)
-								if (!cb.call(ob, Array.prototype.slice.call(n), m))
-									return;
+					if (textNodesOnly && m.target.matches(selector))
+						found.push(m.target);
 					break;
 				case 'attributes':
-					if (m.target.matches(selector) && !cb.call(ob, [m.target], m))
-						return;
+					if (m.target.matches(selector))
+						found.push(m.target);
 					break;
 				case 'characterData':
-					if (m.target.parentNode && m.target.parentNode.matches(selector) && !cb.call(ob, [m.target.parentNode], m))
-						return;
+					if (m.target.parentNode && m.target.parentNode.matches(selector))
+						found.push(m.target.parentNode);
 					break;
 			}
-	});
-	ob.observe(baseNode, options || {subtree:true, childList:true}); 
-	return ob;
+		}
+		if (!found.length)
+			return;
+		if (handler.call(observer, found) === false)
+			observer.disconnect();
+	}
+
+	function MOhandlerForId(mutations) {
+		var el = document.getElementById(selector);
+		if (el && target.contains(el))
+			if (handler.call(observer, [el]) === false)
+				observer.disconnect();
+	}
 }
