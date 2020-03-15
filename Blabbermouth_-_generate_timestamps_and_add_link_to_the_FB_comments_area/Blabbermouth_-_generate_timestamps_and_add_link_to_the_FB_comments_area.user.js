ebf162282e0c379c4d427563e8211a465c4406b3
darkred <rick3162@gmail.com>
2020-03-15 22:40:43 +0200

    (Blabbermouth - generate timestamps and add link to the fb comments area) Now generate timestamps in news listings too, replacing the existing date-only ones

--- Blabbermouth_-_generate_timestamps_and_add_link_to_the_FB_comments_area/Blabbermouth_-_generate_timestamps_and_add_link_to_the_FB_comments_area.user.js
+++ Blabbermouth_-_generate_timestamps_and_add_link_to_the_FB_comments_area/Blabbermouth_-_generate_timestamps_and_add_link_to_the_FB_comments_area.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Blabbermouth - generate timestamps and add link to the fb comments area
 // @namespace   darkred
-// @version     1.0.2
-// @description (blabbermouth cd/dvd reviews and news pages) generates the missing timestamps or converts the existing ones in relative format, and adds link to the fb comments area
+// @version     1.1
+// @description Generates missing timestamps or converts the existing ones in relative format, and adds link to the fb comments area
 // @author      darkred
 // @license     MIT
 // @include     https://www.blabbermouth.net/
@@ -43,23 +43,22 @@ function convertToLocalTimezone(timestamp) {
 	// 2020-03-05T15:40:38.000Z
 	let initialTimestamp = timestamp;
 	if (moment(initialTimestamp, moment.ISO_8601, true).isValid()) {
-		// let convertedToLocalTimezone = moment(initialTimestamp.replace('Z','')  + '-05:00', 'YYYY-MM-DDTHH:mm:ssZ');		// The server's timezone is GMT-5
-		let convertedToLocalTimezone = moment(initialTimestamp.replace('Z','')  + '-04:54', 'YYYY-MM-DDTHH:mm:ssZ');		// The server's timezone is GMT-5 (6 min less, according to the relevant post timestamps in both https://www.facebook.com/Blabbermouth.net and https://twitter.com/BLABBERMOUTHNET
+		// let convertedToLocalTimezone = moment(initialTimestamp.replace('Z','')  + '-05:00', 'YYYY-MM-DDTHH:mm:ssZ');		// the server's timezone is GMT-5
+		let convertedToLocalTimezone = moment(initialTimestamp.replace('Z','')  + '-03:54', 'YYYY-MM-DDTHH:mm:ssZ');		// the server's timezone is GMT-4 (6 min less, in order to sync with the relevant post timestamps in both Twitter and FB blabbbermouth pages)
 		publishedTimeLTZ = convertedToLocalTimezone.fromNow();
 		let format = 'YYYY-MM-DD HH:mm:ss';
 		publishedTimeLTZtitle = convertedToLocalTimezone.format(format);
 	}
 }
 
-function recalc(timestamp, format, notitle) {
-	// repeat every 1 minute
+function recalc(existingTimestampElement, format, notitle) {
 	setInterval(function() {
-		if (timestamp && moment(timestamp.title, format, true).isValid()) {
-			timestamp.textContent = moment(timestamp.title).fromNow();
+		if (existingTimestampElement && moment(existingTimestampElement.title, format, true).isValid()) {
+			existingTimestampElement.textContent = moment(existingTimestampElement.title).fromNow();
 		} else if (notitle === true) {
-			timestamp.innerText = moment(timestamp.innerText.trim()).fromNow();
+			existingTimestampElement.innerText = moment(existingTimestampElement.innerText.trim()).fromNow();
 		}
-	}, 1 * 60 * 1000);
+	}, 1 * 60 * 1000);		// repeat every 1 minute
 }
 
 
@@ -68,38 +67,52 @@ if (
 	window.location.href === 'https://www.blabbermouth.net/news' ||
 	window.location.href.startsWith('https://www.blabbermouth.net/news/page')
 ) {
-	var allTimestamps = document.querySelectorAll('.date-time');
-	// February 22, 2020
-	allTimestamps.forEach(function(item) {
-		var initial = item.innerText.trim();
-		var now = moment();
-		// diff() - The supported measurements are: years, months, weeks, days
-		var interval = now.diff(moment(item.innerText.trim()), 'days');
-		if (interval === 0) {
-			item.innerText = 'Today';
-		} else if (interval < 7) {
-			item.innerText = interval + ' days ago';
-		} else if (interval < 30) {
-			item.innerText =
-				now.diff(moment(item.innerText.trim()), 'weeks') + ' weeks ago';
-		} else if (interval < 365) {
-			item.innerText =
-				now.diff(moment(item.innerText.trim()), 'months') +
-				' months ago';
-		} else {
-			item.innerText =
-				now.diff(moment(item.innerText.trim()), 'years') + ' years ago';
-		}
-		if (
-			interval === 1 ||
-			interval === 7 ||
-			interval === 30 ||
-			interval === 365
-		) {
-			item.innerText = item.innerText.replace('s ', ' ');
-		}
-		item.title = initial;
+
+	let options = {
+		root: null,
+		rootMargin: '0px',
+		threshold: 0
+	};
+
+	let callback = (entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting && !entry.target.classList.contains('in-viewport') ) {
+				entry.target.classList.add('in-viewport');
+
+				var xhr = new XMLHttpRequest();
+				var url = entry.target.parentElement.parentElement.firstElementChild.firstElementChild.href;
+				xhr.open('GET', url, true);	// XMLHttpRequest.open(method, url, async)
+				xhr.onload = function () {
+
+					let container = document.implementation.createHTMLDocument().documentElement;
+					container.innerHTML = xhr.responseText;
+
+					let publishedTimestamp = container.querySelector(
+						'meta[property="article:published_time"]'
+					).content;
+
+					convertToLocalTimezone(publishedTimestamp);
+
+					entry.target.textContent = publishedTimeLTZ;
+					entry.target.title = publishedTimeLTZtitle;
+
+					recalc(entry.target, 'YYYY-MM-DD HH:mm:ss');
+
+				};
+				xhr.send();
+
+			}
+		});
+	};
+
+	let observer = new IntersectionObserver(callback, options);
+
+	var allTimestamps = document.querySelectorAll('span.date-time');
+	allTimestamps.forEach((element) => {
+		observer.observe(element);
 	});
+
+
 } else if (window.location.href.includes('blabbermouth.net/news/')) {
 	if (
 		document.querySelector('meta[property="article:published_time"]') !==
@@ -117,8 +130,14 @@ if (
 
 	convertToLocalTimezone(publishedTimestamp);
 
-	document.querySelector('.date-time').textContent = publishedTimeLTZ;
-	document.querySelector('.date-time').title = publishedTimeLTZtitle;
+	let existingTimestampElement = document.querySelector('.date-time');
+
+	existingTimestampElement.textContent = publishedTimeLTZ;
+	existingTimestampElement.title = publishedTimeLTZtitle;
+
+	recalc(existingTimestampElement, 'YYYY-MM-DD HH:mm:ss');
+
+
 } else if (
 	(window.location.href.includes('blabbermouth.net/cdreviews/') ||
 	window.location.href.includes('blabbermouth.net/dvdreviews/')) &&
--- Blabbermouth_-_generate_timestamps_and_add_link_to_the_FB_comments_area/README.md
+++ Blabbermouth_-_generate_timestamps_and_add_link_to_the_FB_comments_area/README.md
@@ -1,33 +1,34 @@
 This script applies to blabbermouth.net.  
 
-Blabbermouth only displays timestamps in news listings and pages. **It doesn't display timestamps in cd/dvd reviews pages.** 
-The script implements that.
+Blabbermouth only displays timestamps (just the date) in news listings and pages.  
+**It doesn't display timestamps in cd/dvd reviews pages at all.** 
+The script generates timestamps in relative format, in all cases.
 
 In details: 
 
-- in **cd/dvd reviews** pages:  
+- in **news** and **cd/dvd reviews** pages:  
 
   It generates timestamps *(making use of the existing `published_time` data from inside the pages, e.g. `2020-02-20T19:10:22.000Z`)*  
-to relative format in local timezone. Also, it recalculates them every 1 minute.
+in relative format. Also, it recalculates them every 1 minute.  
 
   It also shows a link to the Facebook comments next to the generated timestamp,  
 with **the comment count from that fb iframe** <sup>1</sup> (i.e. `6 Comments`, not just "Comments" ).  
 
 
-- in **news** pages and listings:  
+- in **news** listings:  
 
-  it converts the existing timestamps to releative format.
+  As you scroll down each page, it retrieves the relevant target news page in the background in order to get the relevant `published_time` data from the page, and then generates timestamps (like before) in relative format.  
 
 &nbsp;
 
 <sup>1</sup> Compatibility note regarding that feature:
 
-- Violentmonkey: the script works in ok with default VM settings.  
-- Tampermonkey: `*://www.facebook.com/plugins/*` is blacklisted in TM by default. in order the script to work, you have to remove that pattern from TM blacklist.  
+- Violentmonkey: the script works in ok with VM in its default settings.  
+- Tampermonkey: in order to work with TM, you have to remove the `*://www.facebook.com/plugins/*` pattern from TM blacklist (it's blacklisted in TM settings by default).  
 - Greasemonkey: not supported.
 
 ---
 
-The script uses [moment.js](http://momentjs.com/), [moment-timezone.js](http://momentjs.com/timezone/) and [jsTimezoneDetect](https://bitbucket.org/pellepim/jstimezonedetect) 
+The script uses [moment.js](http://momentjs.com/).
 
 [Hosted in GitHub](https://github.com/darkred/Userscripts)
