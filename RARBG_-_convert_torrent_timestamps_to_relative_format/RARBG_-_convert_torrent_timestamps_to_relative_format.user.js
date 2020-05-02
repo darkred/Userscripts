c8df253f4c7a2c1d2d81663c446995fd35f90503
darkred <rick3162@gmail.com>
2020-05-02 12:27:05 +0300

    (RARBG - convert torrent timestamps to relative format)
    Fix for https://greasyfork.org/en/forum/discussion/77978/

--- RARBG_-_convert_torrent_timestamps_to_relative_format/RARBG_-_convert_torrent_timestamps_to_relative_format.user.js
+++ RARBG_-_convert_torrent_timestamps_to_relative_format/RARBG_-_convert_torrent_timestamps_to_relative_format.user.js
@@ -1,17 +1,33 @@
 // ==UserScript==
 // @name        RARBG - convert torrent timestamps to relative format
 // @namespace   darkred
-// @version     2020.04.04
+// @version     2020.05.02
 // @description Converts torrent upload timestamps to relative format
 // @author      darkred
 // @license     MIT
 // @include     /^(https?:)?\/\/(www\.)?(proxy|unblocked)?rarbg((2018|2019|2020|2021)?|access(ed)?|cdn|core|data|enter|get|go|index|mirror(ed)?|p2p|prox(ied|ies|y)|prx|to(r|rrents)?|unblock(ed)?|way|web)\.(to|com|org|is)\/(torrents\.php.*|catalog\/.*|tv\/.*|top10)$/
 // @grant       none
-// @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.25.0/moment.min.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.28/moment-timezone-with-data-10-year-range.min.js
 // ==/UserScript==
 
-
+'use strict';
 /* global moment */
+
+
+// Based on the timestamp on the footer of each RARBG page --> "Sat, 01 May 2020 20:14:56 +0200",
+// the script takes that the server time is GMT+2 and that it doesn't take DST.
+// Also, the script uses the 'moment-timezone' library as it takes DST offsets into account when converting the timestamps to user/local timezone.
+
+// This is no typo:
+// const serverTimezone = 'Etc/GMT+2';  	// -02:00	-02:00  (=no DST)
+const serverTimezone = 'Etc/GMT-2';  		// +02:00	+02:00  (=no DST)
+
+const localTimezone = moment.tz.guess();    // In my case ----> +02:00	+03:00  (DST)
+
+// const format = 'MM/DD/YYYY HH:mm:ss';
+// const format = 'YYYY-MM-DD HH:mm:ss';
+const format = 'YYYY-MM-DD HH:mm:ss';
 
 // Customize the strings in the locale to display "1 minute ago" instead of "a minute ago" (https://github.com/moment/moment/issues/3764#issuecomment-279928245)
 moment.updateLocale('en', {
@@ -36,17 +52,11 @@ moment.updateLocale('en', {
 function convertToLocalTimezone(timestamps) {
 	for (let i = 0; i < timestamps.length; i++) {
 		let initialTimestamp = timestamps[i].textContent;
-		// if (moment(initialTimestamp, 'YYYY-MM-DD HH:mm:ss').isValid()) {
-		if (moment(initialTimestamp, 'YYYY-MM-DD HH:mm:ss', true).isValid()) {		// As of moment.js v2.3.0, you may specify a boolean for the last argument to make Moment use strict parsing. Strict parsing requires that the format and input match exactly, including delimeters.
-			let convertedToLocalTimezone = moment(initialTimestamp + '+01:00', 'YYYY-MM-DD HH:mm:ss Z');	// The server time is GMT+1
-			if (moment().isDST()){
-				convertedToLocalTimezone.subtract(1, 'hours');
-			}
+		if (moment(initialTimestamp, format, true).isValid()) {		// As of moment.js v2.3.0, you may specify a boolean for the last argument to make Moment use strict parsing. Strict parsing requires that the format and input match exactly, including delimeters.
+			let convertedToLocalTimezone = moment.tz(initialTimestamp, format, serverTimezone).tz(localTimezone);
 			timestamps[i].textContent = convertedToLocalTimezone.fromNow();
-			// let format = 'MM/DD/YYYY HH:mm:ss';
-			let format = 'YYYY-MM-DD HH:mm:ss';
 			timestamps[i].title = convertedToLocalTimezone.format(format);
-			// timestamps[i].title = convertedToLocalTimezone.toISOString(); // 			// Display timestamps in tooltips in ISO 8601 format, combining date and time  (https://stackoverflow.com/questions/25725019/how-do-i-format-a-date-as-iso-8601-in-moment-js/)
+			// timestamps[i].title = convertedToLocalTimezone.toISOString();	// Display timestamps in tooltips in ISO 8601 format, combining date and time  (https://stackoverflow.com/questions/25725019/how-do-i-format-a-date-as-iso-8601-in-moment-js/)
 			// timestamps[i].title = convertedToLocalTimezone.format();
 		}
 	}
