93c53e3ecea688d1226835f8928fca6cd3c167ef
darkred <rick3162@gmail.com>
2020-06-23 18:41:49 +0300

    (ProtonMail - remove forced signature)
    - now also remove the leftover newline above the signature (per https://greasyfork.org/en/scripts/368881-protonmail-remove-forced-signature/discussions/7416)
    - beta.protonmail.com include rule added

--- ProtonMail_-_remove_forced_signature/ProtonMail_-_remove_forced_signature.user.js
+++ ProtonMail_-_remove_forced_signature/ProtonMail_-_remove_forced_signature.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name        ProtonMail - remove forced signature
 // @namespace   darkred
-// @version     1
+// @version     2020.06.23
 // @description Removes the forced ProtonMail signature from the 'New message' textboxes
 // @author      darkred
 // @license     MIT
 // @include     https://mail.protonmail.com/*
+// @include     https://beta.protonmail.com/*
 // @grant       none
 // @require     https://greasyfork.org/scripts/21927-arrive-js/code/arrivejs.js
 
@@ -13,7 +14,7 @@
 
 // wait for the formatting toolbar element to be created
 document.arrive('.squireToolbar-row-1', function () {
-	// select the signature element and empty its innerHTML
-	document.querySelector('.squireIframe').contentDocument.querySelector('div.protonmail_signature_block-proton').innerHTML = '';
-	// document.querySelector('.squireIframe').contentDocument.querySelector('div.protonmail_signature_block-proton').remove();
+	let editorIframe = document.querySelector('.squireIframe').contentDocument;
+	editorIframe.querySelector('.protonmail_signature_block').remove();   // remove the signature element
+	editorIframe.querySelector('body > div:last-child').remove();         // remove (the element that contains a) leftover newline (<br>)
 });
