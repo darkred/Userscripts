532d765215dd2c7b64201588acc28ba7f7e28318
darkred <rick3162@gmail.com>
2020-07-29 21:09:19 +0300

    Update thepiratebay_-_add_a_sortable_Ratio_column.user.js
    - fix #10
    - changed to fixed 1 decimal length (from round-to-1-decimal digit ),
    in order the column to align properly
    - removed the tableSorter require as the result table is now already sortable

--- thepiratebay_-_add_a_sortable_Ratio_column/thepiratebay_-_add_a_sortable_Ratio_column.user.js
+++ thepiratebay_-_add_a_sortable_Ratio_column/thepiratebay_-_add_a_sortable_Ratio_column.user.js
@@ -1,72 +1,45 @@
 // ==UserScript==
 // @name        thepiratebay - add a sortable Ratio column
 // @namespace   darkred
-// @version     1.2.3
-// @date        2018.2.27
+// @date        2020.7.29
 // @description Adds a sortable "Ratio" column
 // @author      darkred
 // @license     MIT
-// @include     https://thepiratebay.org/search/*
-// @include     https://thepiratebay.org/browse/*
-// @include     https://thepiratebay.org/recent
-// @include     https://thepiratebay.org/user/*
-// @include     https://thepiratebay.org/tv/
-// @include     https://thepiratebay.org/music
-// @include     https://thepiratebay.org/top
+// @include     https://thepiratebay.org/search.php*
 // @grant       none
-// @require     https://greasyfork.org/scripts/5844-tablesorter/code/TableSorter.js
+// @require     https://code.jquery.com/jquery-3.5.1.min.js
 // This userscript uses jQuery and it's plugin "tablesorter" (forked by Rob Garrison (Mottie)) http://mottie.github.io/tablesorter/docs/index.html
 // @supportURL  https://github.com/darkred/Userscripts/issues
 // ==/UserScript==
 
 
+// For the sortable Ratio column
 function appendColumn() {
-	var tbl = document.getElementById('searchResult');
-	var newCell;
-	var tr = tbl.tHead.children[0],
-		th = document.createElement('th');
-	// th.innerHTML = 'Ratio';
-	th.innerHTML = 'Rt';
-	th.className = 'ratioCol';
-	tr.appendChild(th);
-	var m, n, ratio;
-	for (var i = 1; i < tbl.rows.length; i++) {
-		m = tbl.rows[i].cells[5].innerHTML;   // Retrieve the content of the cell of the SE column and store it to variable m
-		n = tbl.rows[i].cells[6].innerHTML;   // Retrieve the content of the cell of the LE column and store it to variable n
-		ratio = m/n;
-		if (m > 0 && n === 0){
-			ratio=m;
-		}
-		if (m === 0 && n === 0){
+	var se, le, ratio;
+	if (document.querySelector('span.list-header:nth-child(7)')){
+		document.querySelector('span.list-header:nth-child(7)').insertAdjacentHTML('afterend', '<span class="list-item list-header item-seed"><label onclick="sortlist(7);" title="Seeds/Peers">Rt</label></span>');
+	}
+	var entries = document.querySelectorAll('li.list-entry > span:nth-child(7)');
+	for (var i = 0; i < entries.length; i++) {
+		se = parseInt(entries[i].previousElementSibling.innerHTML);   // Retrieve the content of the cell of the SE column and store it to variable se
+		le = parseInt(entries[i].innerHTML);   // Retrieve the content of the cell of the LE column and store it to variable le
+		if (se > 0 && le === 0){
+			ratio = se;
+		} else if (se === 0 || le === 0){
 			ratio = 0;
+		} else {
+			ratio = se/le;
 		}
-		newCell = tbl.rows[i].insertCell(-1);
-		newCell.innerHTML = (Math.round(100 * ratio) / 100);
-
+		// ratio = (Math.round(10 * ratio) / 10);
+		ratio = ratio.toFixed(1);
+		entries[i].insertAdjacentHTML('afterend', '<span>' + ratio + '</span>');
+		entries[i].nextSibling.className = 'list-item item-ratio';
+		$('.item-ratio').css('text-align', 'right');
 	}
 }
 
-
-
-function sorting() {
-	$('table#searchResult').tablesorter({
-		headers: {
-			0: {sorter: false},
-			1: {sorter: false},
-			2: {sorter: false},
-			3: {sorter: false},
-			4: {sorter: false},
-			5: {sorter: false},
-			6: {sorter: false},
-			7: {sorter: true}
-		}
-	});
-}
-//
-
 appendColumn();
-sorting();
 
 
 // Select all Ratio values (=all last cells of all rows) an align to the text
-$('td:last-child').css('text-align', 'right');
+$('.list-item:last-child').css('text-align', 'right');
