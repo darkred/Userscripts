dbd2932e3b6eb911d03da2abbbee76ea1e77cdaf
darkred <rick3162@gmail.com>
2020-07-30 13:27:33 +0300

    Update thepiratebay_helper.user.js
    - fixed appendColumn() (removed no longer needed tableSorter require)
    - fixed script Settings creation position
    - fixed convertDates() (now the site's only date format) is YYYY-MM-DD)

--- thepiratebay_helper/thepiratebay_helper.user.js
+++ thepiratebay_helper/thepiratebay_helper.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        thepiratebay helper
 // @namespace   darkred
-// @version     0.9.6n
-// @date        2018.5.4
+// @version     0.9.7
+// @date        2020.7.30
 // @description Converts dates to local timezone on thepiratebay and optionally either highlight VIP/Trusted/Moderator/Helper torrents or hide non verified torrents altogether
 // @authors     emptyparad0x, darkred
 // @license     MIT
@@ -12,7 +12,6 @@
 // @require     https://greasyfork.org/scripts/28536-gm-config/code/GM_config.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/keypress/2.1.4/keypress.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js
-// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.28.5/js/jquery.tablesorter.min.js
 // @run-at      document-idle
 // @supportURL  https://github.com/darkred/Userscripts/issues
 // ==/UserScript==
@@ -20,12 +19,12 @@
 /* global GM_config, moment */
 
 
-
+/*
 if (document.querySelector('.viewswitch > a:nth-child(1)') && document.querySelector('.viewswitch > a:nth-child(1)').outerHTML.indexOf('Single</a>') !== -1 ) {
 	$('body').append(`<center>In order thepiratebay helper to work, please switch the view mode of the search results to single-line <i>(click: 'Single' in the table header, and then reload the page (F5)</i>)</center>`);
 	throw new Error('Not in single-line view mode. The usercript exits.');
 }
-
+*/
 
 
 
@@ -64,12 +63,12 @@ GM_config.init('TPB Helper settings',{
 	keepNonTrustedWithComments: {label: '...when toggle, include those non-trusted which have comments:', type: 'checkbox', default: true},
 	relativeDates: {label: 'Display torrent timestamps in relative format:', type: 'checkbox', default: true},
 	swapVerifiedIconsWithComments: {label: 'Swap the verified icons with the Comments icon:', type: 'checkbox', default: true},
-	sortableRtColumn: {label: 'Add a sortable Ratio column?', type: 'checkbox', default: true}
+	RtColumn: {label: 'Add a Ratio column?', type: 'checkbox', default: true}
 },{
 	save: function(){location.reload();}
 });
 
-$('body').append(`<center><a id='TimeChangerConfig'>TPB Helper settings</a></center>`);
+$('footer').append(`<center><a id='TimeChangerConfig'>TPB Helper settings</a></center>`);
 $('#TimeChangerConfig').click(function(){GM_config.open();}).css({'cursor': 'pointer'});
 
 // var timezone = GM_config.get('timezone');
@@ -77,7 +76,7 @@ var tpboffset = GM_config.get('tpboffset');
 var enhanceVisibility = GM_config.get('enhanceVisibility');
 var keepNonTrustedWithComments = GM_config.get('keepNonTrustedWithComments');
 var relativeDates = GM_config.get('relativeDates');
-var sortableRtColumn = GM_config.get('sortableRtColumn');
+var RtColumn = GM_config.get('RtColumn');
 var swapVerifiedIconsWithCommentsChoice = GM_config.get('swapVerifiedIconsWithComments');
 
 
@@ -85,13 +84,13 @@ var swapVerifiedIconsWithCommentsChoice = GM_config.get('swapVerifiedIconsWithCo
 
 
 
-// For the sortable Ratio column
+// For the Ratio column
 function appendColumn() {
 	var se, le, ratio;
-	if (document.querySelector('.header > th:nth-child(7)')){
-		document.querySelector('.header > th:nth-child(7)').insertAdjacentHTML('afterend', '<th><abbr title="Seeders">Rt</abbr></th>');
+	if (document.querySelector('span.list-header:nth-child(7)')){
+		document.querySelector('span.list-header:nth-child(7)').insertAdjacentHTML('afterend', '<span class="list-item list-header item-seed"><label onclick="sortlist(7);" title="Seeds/Peers">Rt</label></span>');
 	}
-	var entries = document.querySelectorAll('#searchResult > tbody:nth-child(2) > tr > td:nth-child(7)');
+	var entries = document.querySelectorAll('li.list-entry > span:nth-child(7)');
 	for (var i = 0; i < entries.length; i++) {
 		se = parseInt(entries[i].previousElementSibling.innerHTML);   // Retrieve the content of the cell of the SE column and store it to variable se
 		le = parseInt(entries[i].innerHTML);   // Retrieve the content of the cell of the LE column and store it to variable le
@@ -102,29 +101,20 @@ function appendColumn() {
 		} else {
 			ratio = se/le;
 		}
-		ratio = (Math.round(10 * ratio) / 10);
-		entries[i].insertAdjacentHTML('afterend', '<td>' + ratio + '</td>');
-		entries[i].nextSibling.className = 'ratioCol';
-		$('.ratioCol').css('text-align', 'right');
+		// ratio = (Math.round(10 * ratio) / 10);
+		ratio = ratio.toFixed(1);
+		entries[i].insertAdjacentHTML('afterend', '<span>' + ratio + '</span>');
+		entries[i].nextSibling.className = 'list-item item-ratio';
+
+		$('.item-ratio').css('text-align', 'right');
 	}
-
-	$('table#searchResult').tablesorter({
-		headers: {
-			0: {sorter: false},
-			1: {sorter: false},
-			2: {sorter: false},
-			3: {sorter: false},
-			4: {sorter: false},
-			5: {sorter: false},
-			6: {sorter: false},
-			7: {sorter: true}
-		}
-	});
-
 }
 
-if (sortableRtColumn === true) {
+if (RtColumn === true) {
 	appendColumn();
+	$('.list-item:last-child').css('text-align', 'right');  	// Select all Ratio values (=all last cells of all rows) and align them to the right
+
+
 }
 
 
@@ -349,9 +339,12 @@ function highlight() {
 
 
 
+// 2020-07-30
+
 function convertDates() {
 	// alert();
-	var dates = document.querySelectorAll('#searchResult > tbody > tr > td:nth-child(3)');
+	// var dates = document.querySelectorAll('#searchResult > tbody > tr > td:nth-child(3)');
+	var dates = document.querySelectorAll('#st > span.list-item.item-uploaded');
 	for (var i = 0; i < dates.length; i++) {
 		if (dates[i].title === '' ) {		// if it's the 1st time the function is called
 			var initial = dates[i].innerHTML.replace(/&nbsp;/g, ' ').replace(/<\/?b>/g, '');
@@ -364,6 +357,7 @@ function convertDates() {
 		// then they are displayed in relative format (in bold) i.e. already in GMT+1
 		// instead of like this e.g. "Today 22:10", i.e. GMT, therefore it needs to be converted to GMT+1
 		// 1 min ago
+		/*
 		if (temp.indexOf('mins ago') !== -1 || temp.indexOf('min ago') !== -1) {
 			temp = moment().subtract(parseInt(temp), 'minutes');
 			dates[i].setAttribute('relative', 'true');
@@ -403,9 +397,13 @@ function convertDates() {
 			dates[i].setAttribute('minus1day', 'true');
 		}
 
+		*/
+
+		temp = moment(temp, 'YYYY-MM-DD', true);
 
 		dates[i].innerHTML = temp.fromNow();
-		dates[i].title = temp.format('MM-DD-YYYY HH:mm');
+		// dates[i].title = temp.format('MM-DD-YYYY HH:mm');
+		dates[i].title = temp.format('YYYY-MM-DD');
 
 
 	}
