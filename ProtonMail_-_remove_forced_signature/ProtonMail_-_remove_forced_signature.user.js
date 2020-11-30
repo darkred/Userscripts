4dabca1fae564bea8b0f41e93d935d6f91c776d1
darkred <rick3162@gmail.com>
2020-12-01 00:45:06 +0200

    Update ProtonMail_-_remove_forced_signature.user.js
    Fix https://github.com/darkred/Userscripts/issues/13

--- ProtonMail_-_remove_forced_signature/ProtonMail_-_remove_forced_signature.user.js
+++ ProtonMail_-_remove_forced_signature/ProtonMail_-_remove_forced_signature.user.js
@@ -1,20 +1,62 @@
 // ==UserScript==
 // @name        ProtonMail - remove forced signature
 // @namespace   darkred
-// @version     2020.30.11
+// @version     2020.12.01
 // @description Removes the forced ProtonMail signature from the 'New message' textboxes
 // @author      darkred
 // @license     MIT
 // @include     https://mail.protonmail.com/*
+// @include     https://beta.protonmail.com/*
 // @grant       none
 // @require     https://greasyfork.org/scripts/21927-arrive-js/code/arrivejs.js
 // @supportURL  https://github.com/darkred/Userscripts/issues
 // @icon        https://protonmail.com/images/favicon.ico
 // ==/UserScript==
 
+const isInBeta = window.location.href.includes('beta');
+
+var elementToWatch;
+isInBeta? elementToWatch = '.squireIframe' : elementToWatch = '.squireToolbar-row-1';
+
+
 // wait for the formatting toolbar element to be created
-document.arrive('.squireToolbar-row-1', function () {
-	let editorIframe = document.querySelector('.squireIframe').contentDocument;
-	editorIframe.querySelector('.protonmail_signature_block').remove();   // remove the signature element
-	editorIframe.querySelector('body > div:last-child').remove();         // remove (the element that contains a) leftover newline (<br>)
+document.arrive(elementToWatch, function () {
+
+	// debugger
+
+	let iframe;
+
+
+
+	if (!isInBeta) {
+
+		iframe = document.querySelector('.squireIframe').contentDocument;
+		iframe.querySelector('.protonmail_signature_block').remove();   // remove the signature element
+		iframe.querySelector('body > div:last-child').remove();         // remove (the element that contains a) leftover newline (<br>)
+
+	} else {
+
+		iframe = this.contentDocument;
+
+		// Setup the config
+		const config = {
+			childList: true,
+			subtree: true
+		};
+
+		const callback = function callback(mutationList, observer) {
+			mutationList.forEach( (mutation) => {
+				mutation.addedNodes.forEach( (node) => {
+					if (node.className === 'protonmail_signature_block') {
+						node.remove();
+						observer.disconnect();
+					}});
+
+			});
+		};
+
+		// Watch the iframe for changes
+		const observer = new MutationObserver(callback);
+		observer.observe(iframe, config);
+	}
 });
