2ecd2cf7a3f704be7db4bcb154ac328844656414
darkred <rick3162@gmail.com>
2020-11-30 21:22:30 +0200

    Update ProtonMail_-_remove_forced_signature.user.js
    Removed support for beta.protonmail.com
    for the time being because of https://github.com/darkred/Userscripts/issues/13

--- ProtonMail_-_remove_forced_signature/ProtonMail_-_remove_forced_signature.user.js
+++ ProtonMail_-_remove_forced_signature/ProtonMail_-_remove_forced_signature.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name        ProtonMail - remove forced signature
 // @namespace   darkred
-// @version     2020.06.29
+// @version     2020.30.11
 // @description Removes the forced ProtonMail signature from the 'New message' textboxes
 // @author      darkred
 // @license     MIT
 // @include     https://mail.protonmail.com/*
-// @include     https://beta.protonmail.com/*
 // @grant       none
 // @require     https://greasyfork.org/scripts/21927-arrive-js/code/arrivejs.js
 // @supportURL  https://github.com/darkred/Userscripts/issues
