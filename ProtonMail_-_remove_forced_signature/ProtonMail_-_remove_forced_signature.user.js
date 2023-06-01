995a514ca5697805f7b92d3035527f2839b7567d
darkred <rick3162@gmail.com>
2023-06-01 23:00:25 +0300

    Update ProtonMail_-_remove_forced_signature.user.js
    Updated for the recent html changes. Per https://greasyfork.org/en/scripts/368881-protonmail-remove-forced-signature/discussions/185640

--- ProtonMail_-_remove_forced_signature/ProtonMail_-_remove_forced_signature.user.js
+++ ProtonMail_-_remove_forced_signature/ProtonMail_-_remove_forced_signature.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        ProtonMail - remove forced signature
 // @namespace   darkred
-// @version     2022.7.11
+// @version     2023.6.1
 // @description Removes the forced ProtonMail signature from the 'New message' textboxes
 // @author      darkred
 // @license     MIT
@@ -27,33 +27,44 @@ document.arrive(elementToWatch, function () {
 	const callback = function(mutationList, observer) {
 		mutationList.forEach( (mutation) => {
 			mutation.addedNodes.forEach( (node) => {
-				if (node.className === 'protonmail_signature_block') {
+
+				const refNode = 'protonmail_signature_block';
+
+				if (node.className === refNode) {
 
 					// DOM STRUCTURE:
 					//
-					// The 2 previous element siblings of the main signature element, '.protonmail_signature_block', are both <div> elements with <br>:
-					//                                  <br>  | <br> | .protonmail_signature_block
+					// The 2 previous element siblings of the main signature element, '.protonmail_signature_block', both contain a <div> element with a <br> inside, i.e. <br>  =   <div> \ <br>  ) :
+					//                                  <div> \ <br>  |  <div> \ <br>  |  .protonmail_signature_block
 					//
-					// The '.protonmail_signature_block' itself has 3 <div> children which can be:
-					// (when user signature doesn't exist) 2 with <br>, and the proton signature
-					//                                  <br>  | <br> | .protonmail_signature_block-proton
-					// (when user signature exists) the user signature (.protonmail_signature_block-user) , 1 with <br> , and the signature itself
-					//       .protonmail_signature_block-user | <br> | .protonmail_signature_block-proton
+					// The '.protonmail_signature_block' itself has either 3 or 2 <div> children which can be:
+					//
+					// (when user signature doesn't exist) the (empty) user signature , and the proton signature
+					// .protonmail_signature_block-user.protonmail_signature_block-empty | .protonmail_signature_block-proton
+					//
+					// (when user signature exists) the user signature , 1 <div> \ <br> , and the signature itself
+					//       .protonmail_signature_block-user                    |  <div> \ <br>  |  .protonmail_signature_block-proton
 					//
 					// The script's functionality is:
-					// 1. if a user signature exists, to also remove the 2 <br> elements before the main '.protonmail_signature_block' element.
-					// 2. Regardless of whether a user signature exists or not, to remove the last element ('.protonmail_signature_block-proton') and the <br> before it.
+					// 1. Regardless of whether a user signature exists or not, to remove the 2 <div> \ <br> elements before the main/reference '.protonmail_signature_block' element.
+					// 2a. to remove the last element ('.protonmail_signature_block-proton')
+					// 2b.   +  if a user signature is not empty, to also remove the <div> \ <br> before it.
+					//
+					// See DOM screenshots: https://imgur.com/a/VEI4nDQ
 
-					const signatureUser   = '.protonmail_signature_block-user';
+
 					const signatureProton = '.protonmail_signature_block-proton';
 
-					if (node.querySelector(signatureUser).firstElementChild.textContent === '' ) {
+
+					if (!node.firstElementChild.classList.contains('protonmail_signature_block-empty')){
+						node.querySelector(signatureProton).previousElementSibling.remove();
+					} else {
 						node.previousElementSibling.remove();
 						node.previousElementSibling.remove();
 					}
 
-					node.querySelector(signatureProton).previousElementSibling.remove();
 					node.querySelector(signatureProton).remove();
+
 
 					observer.disconnect();
 				}
